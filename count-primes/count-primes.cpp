ef6c8351f06acc1c5ec8619c222d4aee34e1e12b
Arup <37553488+ArupDas15@users.noreply.github.com>
2021-06-19 16:28:18 +0530

    Update count-primes.cpp

--- count-primes/count-primes.cpp
+++ count-primes/count-primes.cpp
@@ -4,12 +4,12 @@ public:
     int countPrimes(int n) {
         // Initialize a boolean array from 0 to n
         vector<bool> isPrime(n+1,true);
-        long long int count = 0;
+        int count = 0;
 
-        for (long long int i = 2; i < n; i++){
+        for (long int i = 2; i < n; i++){
             if(isPrime[i]){
                 count = count + 1;
-                for (long long int j = i * i; j < n; j=j+i){
+                for (long int j = i * i; j < n; j=j+i){
                     isPrime[j] = false;
                 }
             }
