06c131cfbb96eb13e828df5442e2be52c78be2e6
Arup <37553488+ArupDas15@users.noreply.github.com>
2021-06-26 00:18:06 +0530

    Added approach details.

--- maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -1,3 +1,13 @@
+/* APPROACH 
+1) Sort input array in increasing order. 
+2) If all elements are positive, then return the product of the last two numbers. 
+3) Else return a maximum of products of the first two and last two numbers. 
+Time complexity: O(nlogn)
+Space complexity: O(1)
+The sort() internally uses IntroSort. In more details it is implemented using hybrid of QuickSort, HeapSort and InsertionSort.
+By default, it uses QuickSort but if QuickSort is doing unfair partitioning and taking more than N*logN time, it switches to HeapSort 
+and when the array size becomes really small, it switches to InsertionSort.
+*/
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
