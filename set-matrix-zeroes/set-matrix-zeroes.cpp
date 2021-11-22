e2368174e06e26b3f114f9d74471d27b08e3033b
Arup <37553488+ArupDas15@users.noreply.github.com>
2021-11-23 00:02:59 +0530

    Time: 24 ms (12.82%), Space: 13.3 MB (53.74%) - LeetHub

--- set-matrix-zeroes/set-matrix-zeroes.cpp
+++ set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector <int> row_list, col_list;
-        for (int j=0;j<matrix[0].size();j++){
-              long double col_prod = 1;
-              for(int i=0;i<matrix.size();i++){
-                  col_prod=col_prod*matrix[i][j];
-                  if(col_prod==0){
-                      col_list.push_back(j);
-                      break;
-                  }
-              }
+        bool is_Col = false;
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        
+        for (int i=0;i<rows;i++){
+            if(matrix[i][0]==0){
+                    is_Col = true;
+                }
+            for(int j=1;j<cols;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+                
+            }
         }
-        for (int i=0;i<matrix.size();i++){
-            long double row_prod=1;
-            for(int j=0;j<matrix[0].size();j++){
-                row_prod=row_prod*matrix[i][j];
-                if(row_prod==0){
-                    row_list.push_back(i);
-                    break;
+        for(int i=1;i<rows;i++){
+            for(int j=1;j<cols;j++){
+                if(!matrix[i][0]||!matrix[0][j]){
+                    matrix[i][j]=0;
                 }
             }
         }
-        for (int indices=0;indices<row_list.size();indices++){
-            for(int i=0;i<matrix[0].size();i++){
-                matrix[row_list[indices]][i]=0;
+        if(matrix[0][0]==0){
+            for(int j=0;j<cols;j++){
+                matrix[0][j]=0;
             }
         }
-        for (int indices=0;indices<col_list.size();indices++){
-            for(int i=0;i<matrix.size();i++){
-                matrix[i][col_list[indices]]=0;
+        if(is_Col){
+                for(int i=0;i<rows;i++){
+                matrix[i][0]=0;
             }
         }
-    }
+        
+        }
 };
