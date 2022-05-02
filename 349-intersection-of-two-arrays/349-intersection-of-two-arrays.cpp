39a84091cd97425435808431a50672e39535e898
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-02 15:09:54 +0530

    Time: 16 ms (15.25%), Space: 10.4 MB (54.04%) - LeetHub

---
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+       unordered_map<int,int> umap;
+        vector<int>res;
+    int n=nums1.size(),m=nums2.size();
+    for(int i=0;i<n;i++){
+			    
+				    umap[nums1[i]] = 1;
+				
+			}
+			for(int i=0;i<m;i++){
+			    if (umap.find(nums2[i])!= umap.end()){
+                    if(umap[nums2[i]]==1)
+                    {res.push_back(nums2[i]);
+                     umap[nums2[i]]=3;
+                    }
+                    			}
+            }
+        
+    return res;
+    }
+};
