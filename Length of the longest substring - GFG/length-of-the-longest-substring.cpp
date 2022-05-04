26af9aafa57befaec6c22c1821d00d051bb8cf83
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-04 13:03:44 +0530

    Added solution - LeetHub

--- Length of the longest substring - GFG/length-of-the-longest-substring.cpp
+++ Length of the longest substring - GFG/length-of-the-longest-substring.cpp
@@ -6,17 +6,42 @@ using namespace std;
  // } Driver Code Ends
 class Solution{
     public:
-    int longestUniqueSubsttr(string s){
-       vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+    int longestUniqueSubsttr(string str){
+     int n = str.length();
+        int cur_len = 1;  // lenght of current substring
+        int max_len = 1;  // result
+        int prev_index;  //  previous index
+        int i;
+        int visited[26];
+     
+        for (i = 0; i < 26;  i++)
+            visited[i] = -1;
+        // cout<<(int)str[0]<<"\n";
+        visited[str[0]-'a'] = 0;
+     
+        for (i = 1; i < n; i++)
+        {
+            prev_index =  visited[str[i]-'a'];
+     
+            if (prev_index == -1 || i - cur_len > prev_index)
+                cur_len++;
+     
+            else
+            {
+                if (cur_len > max_len)
+                    max_len = cur_len;
+     
+                cur_len = i - prev_index;
+            }
+     
+            visited[str[i]-'a'] = i;
         }
-        return maxLen;
-        
+     
+        if (cur_len > max_len)
+            max_len = cur_len;
+     
+        // free(visited); // free memory allocated for visited
+        return max_len;   
     }
 };
 
