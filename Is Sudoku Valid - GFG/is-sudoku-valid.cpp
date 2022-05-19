5923e5cf62508a2843f4cc36fedfbde8125502f0
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-19 10:09:57 +0530

    Added solution - LeetHub

--- Is Sudoku Valid - GFG/is-sudoku-valid.cpp
+++ Is Sudoku Valid - GFG/is-sudoku-valid.cpp
@@ -15,19 +15,19 @@ public:
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9 ; j++){
                 if(mat[i][j]!=0){
-                    key = char(mat[i][j])+ std::string(" found in row ")+to_string(i);
+                    key = to_string(mat[i][j])+ std::string(" found in row ")+to_string(i);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
                     else 
                         return false;
-                    key = char(mat[i][j])+ std::string(" found in column ")+to_string(j);
+                    key = to_string(mat[i][j])+ std::string(" found in column ")+to_string(j);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
                     else 
                         return false;
-                    key = char(mat[i][j])+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
+                    key = to_string(mat[i][j])+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
