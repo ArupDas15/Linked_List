9cbfc44c773d67c89504cc5d0ab9ee7c5d3a6f7e
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-19 10:07:27 +0530

    Time: 51 ms (12.13%), Space: 22.8 MB (5.32%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& mat) {
-       int r[9][9], c[9][9], s[3][3][9];
-        memset(r, 0, sizeof(r));
-        memset(c, 0, sizeof(c));
-        memset(s, 0, sizeof(s));
-        int current_val;
-        int number;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (mat[i][j]=='.'){
-                    current_val = 0;
+       unordered_set<string> seen;
+        string key;
+        string current_val;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9 ; j++){
+                // current_val = to_string(mat[i][j]);
+                // if(current_val!=std::string(".")){
+                if(mat[i][j]!='.'){
+                    key = mat[i][j]+ std::string(" found in row ")+to_string(i);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
+                    key = mat[i][j]+ std::string(" found in column ")+to_string(j);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
+                    key = mat[i][j]+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
                 }
-                else current_val = mat[i][j]-'0';
-                if (current_val > 0 && current_val <= 9) {
-                    number = current_val - 1;
-                    if (r[i][number]) 
-                        return false;
-                    r[i][number] = 1;
-                    if (c[j][number]) 
-                        return false;
-                    c[j][number] = 1;
-                    // checking if same box have repeated value except for 0
-                    if (s[i / 3][j / 3][number]) 
-                        return false;
-                    s[i / 3][j / 3][number] = 1;
-                }
+                
             }
-        }
+        } 
         return true;
+    
     }
 };
