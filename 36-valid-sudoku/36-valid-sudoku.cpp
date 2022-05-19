3058097e77944426580391aa74177ddcb6f3ab7d
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-19 11:59:35 +0530

    Time: 55 ms (9.19%), Space: 22.6 MB (5.32%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -4,29 +4,29 @@ public:
        unordered_set<string> seen;
         string key;
         string current_val;
-        for(int i =0;i < 9; i++){
+        for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9 ; j++){
-                if(mat[i][j]!='.'){
-                    key = to_string(mat[i][j])+ std::string(" found in row ")+to_string(i);
+                current_val = to_string(mat[i][j]);
+                if(current_val!="46"){
+                    key = current_val+ std::string(" found in row ")+to_string(i);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
                     else 
                         return false;
-                    key = to_string(mat[i][j])+ std::string(" found in column ")+to_string(j);
+                    key = current_val+ std::string(" found in column ")+to_string(j);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
                     else 
                         return false;
-                    key = to_string(mat[i][j])+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
+                    key = current_val+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
                     if(seen.find(key) == seen.end()){
                         seen.insert(key);
                     }
                     else 
                         return false;
                 }
-                
             }
         } 
         return true;
