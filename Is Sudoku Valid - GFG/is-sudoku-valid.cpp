ae4fe5c2c7c0f7457b95ddb19beeb47fd308a336
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-05-19 10:09:18 +0530

    Added solution - LeetHub

--- Is Sudoku Valid - GFG/is-sudoku-valid.cpp
+++ Is Sudoku Valid - GFG/is-sudoku-valid.cpp
@@ -10,29 +10,35 @@ using namespace std;
 class Solution{
 public:
     int isValid(vector<vector<int>> mat){
-        int r[9][9], c[9][9], s[3][3][9];
-        memset(r, 0, sizeof(r));
-        memset(c, 0, sizeof(c));
-        memset(s, 0, sizeof(s));
-        int number;
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (mat[i][j] > 0 && mat[i][j] <= 9) {
-                    number = mat[i][j] - 1;
-                    if (r[i][number]) 
-                        return 0;
-                    r[i][number] = 1;
-                    if (c[j][number]) 
-                        return 0;
-                    c[j][number] = 1;
-                    // checking if same box have repeated value except for 0
-                    if (s[i / 3][j / 3][number]) 
-                        return 0;
-                    s[i / 3][j / 3][number] = 1;
+        unordered_set<string> seen;
+        string key;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9 ; j++){
+                if(mat[i][j]!=0){
+                    key = char(mat[i][j])+ std::string(" found in row ")+to_string(i);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
+                    key = char(mat[i][j])+ std::string(" found in column ")+to_string(j);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
+                    key = char(mat[i][j])+ std::string(" found in box ")+to_string(i/3)+to_string(j/3);
+                    if(seen.find(key) == seen.end()){
+                        seen.insert(key);
+                    }
+                    else 
+                        return false;
                 }
+                
             }
-        }
-        return 1;
+        } 
+        return true;
+    
     }
 };
 
