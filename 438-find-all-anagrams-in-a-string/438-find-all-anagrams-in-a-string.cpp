aea26740aaf1b5475927ff04d8393df574f39b98
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-06 20:23:19 +0530

    Time: 45 ms (26.51%), Space: 8.7 MB (74.22%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -15,24 +15,21 @@ public:
                     if(um[s[j]]==0)
                         count--;
                 }
-            if(j-i+1<k){
-                
-                j++;
-            }
+            if(j-i+1<k)   
+                j++;       
             else if(j-i+1==k){
-                if(count==0){
+                if(count==0)
                     ans.push_back(i);
-                }
+                
                 if(um.find(s[i])!=um.end()){
-                    if(um[s[i]]==0){
+                    if(um[s[i]]==0)
                         count++;
-                    }
                     um[s[i]]++;
                 }
+                
                 i++;
                 j++;
             }
-                
         }
         
         return ans;
