2a9f56c4b07c3d952c85f4471a1cb9e9a298386d
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-08 00:40:45 +0530

    Time: 37 ms (35.48%), Space: 9 MB (35.48%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,13 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> dict(256, -1);
-        int maxLen = 0, start = -1;
-        for (int i = 0; i != s.length(); i++) {
-            if (dict[s[i]] > start)
-                start = dict[s[i]];
-            dict[s[i]] = i;
-            maxLen = max(maxLen, i - start);
+        int i=0;// start index of window.
+        int j=0;// end index of window.
+        unordered_map<char,int> um;
+        int maxLen=0;// to store the ans
+        while(j<s.length()){
+            um[s[j]]++;
+            // Check if all the characters in the window are unique.
+            // This will happen only when size of the window and the map are equal.
+            if(um.size()==j-i+1){
+                maxLen = max(maxLen, j-i+1);
+            }
+            else if(um.size()<j-i+1){
+                while(um.size()<j-i+1){
+                    um[s[i]]--;
+                    if(um[s[i]]==0)
+                        um.erase(s[i]);
+                    i++;
+                }
+            }
+            j++;
         }
         return maxLen;
     }
