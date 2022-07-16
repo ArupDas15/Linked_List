42636dc7dfdbad7a4f4fb8d1471bbdce699a1e43
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-16 19:38:51 +0530

    Time: 13 ms (42.04%), Space: 11.4 MB (54.50%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-       void solve(string ip, string op, set<string>& ans){
+       void solve(string ip, string op,vector<string>& ans){
         if(ip.length()==0){
-            ans.insert(op);
+            ans.push_back(op);
             return;
         }
         string op1=op;
@@ -10,23 +10,21 @@ public:
         if(isalpha(ip[0])){
             op1.push_back(tolower(ip[0]));
             op2.push_back(toupper(ip[0]));
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,ans);
+            solve(ip,op2,ans);
         }
         else{
             op1.push_back(ip[0]);
             op2.push_back(ip[0]);
+            ip.erase(ip.begin()+0);
+            solve(ip,op1,ans);
         }
-        ip.erase(ip.begin()+0);
-        solve(ip,op1,ans);
-        solve(ip,op2,ans);
     }
     vector<string> letterCasePermutation(string s) {
         string op="";
         vector<string> ans;
-        set<string>ans_set;
-        solve(s,op,ans_set);
-        for(auto itr : ans_set){
-            ans.push_back(itr);
-        }
+        solve(s,op,ans);
         return ans; 
     }
 };
