309a051caf52f60898a25421e47c848f9b16f4ae
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-21 23:22:28 +0530

    Time: 38 ms (50.30%), Space: 7 MB (96.73%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int m = coins.size();
-        int t[m + 1][amount + 1];
-        for( int i = 0; i < m + 1; i++){
-            t[i][0] = 1;
-        }
-        for(int i = 0; i < amount + 1; i++){
-            t[0][i] = 0;
-        }
-        t[0][0] = 1;
-        for(int i = 1; i < m + 1; i++){
+        vector<int> t(amount + 1, 0);
+        t[0] = 1;
+        for(int i = 1; i < coins.size() + 1; i++){
             for(int j = 1; j < amount + 1; j++){
                 if(coins[i-1] <= j){
-                    t[i][j] = t[i][j - coins[i-1]] + t[i - 1][j];
+                    t[j] = t[j - coins[i-1]] + t[j];
                 }
-                else
-                    t[i][j] = t[i - 1][j];
+                
             }
         }
-        return t[m][amount];
+        return t[amount];
+        // int m = coins.size();
+        // int t[m + 1][amount + 1];
+        // for( int i = 0; i < m + 1; i++){
+        //     t[i][0] = 1;
+        // }
+        // for(int i = 0; i < amount + 1; i++){
+        //     t[0][i] = 0;
+        // }
+        // t[0][0] = 1;
+        // for(int i = 1; i < m + 1; i++){
+        //     for(int j = 1; j < amount + 1; j++){
+        //         if(coins[i-1] <= j){
+        //             t[i][j] = t[i][j - coins[i-1]] + t[i - 1][j];
+        //         }
+        //         else
+        //             t[i][j] = t[i - 1][j];
+        //     }
+        // }
+        // return t[m][amount];
     }
 };
