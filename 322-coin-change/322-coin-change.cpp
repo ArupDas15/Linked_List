934a94b573ca93ccfab53ea1cd792e9ffa8f9e6f
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-25 06:59:15 +0530

    Time: 135 ms (51.05%), Space: 10.1 MB (93.38%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int t[amount + 1];
-	    sort(coins.begin(), coins.end());
+	    // sort(coins.begin(), coins.end());
 	    t[0] = 0;
 	    for(int i = 1; i < amount + 1; i++){
 	        t[i] = INT_MAX;
@@ -11,7 +11,7 @@ public:
 	                if(t[i-coins[c]] != INT_MAX)
 	                    t[i] = min(t[i],t[i-coins[c]]+1);
 	            }
-	            else break;
+	            // else break;
 	        }
 	    }
 	    return t[amount] == INT_MAX? -1 : t[amount];
