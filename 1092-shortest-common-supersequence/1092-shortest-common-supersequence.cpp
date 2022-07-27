a593b63d8547992c646d01a89048e9acc390b3ad
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-27 23:49:44 +0530

    Time: 18 ms (74.68%), Space: 17.5 MB (12.05%) - LeetHub

---
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    string LCS(string str1, string str2){
+        int m = str1.length(), n = str2.length();
+        int t[m+1][n+1];
+        for(int i = 0; i < m + 1; i++){
+            for( int j = 0; j < n + 1; j++){
+                if(i == 0 || j == 0)
+                    t[i][j] = 0;
+                else{
+                    if(str1[i - 1] == str2[j - 1])
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    else
+                        t[i][j] = max (t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        int i = m, j = n;
+        string ans;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans = ans + str1[i - 1];
+                i--;
+                j--;
+            }
+            else if(t[i-1][j] > t[i][j-1])
+                i--;
+            else j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
+    }
+    string shortestCommonSupersequence(string str1, string str2) {
+        int i = 0, j = 0;
+        string res="";
+        for(char c:LCS(str1,str2)){
+            while(str1[i]!=c)
+                res=res+str1[i++];
+            while(str2[j]!=c)
+                res=res+str2[j++];
+            res=res+c;
+            i++;
+            j++;
+        }
+        res=res+str1.substr(i)+str2.substr(j);
+        return res;
+    }
+};
