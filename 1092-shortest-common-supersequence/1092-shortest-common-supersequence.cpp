c3f2d1138b3cc8fcdb6c000ac5d8cd740655dcaf
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-07-27 23:51:14 +0530

    Time: 9 ms (95.51%), Space: 17.7 MB (11.99%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     string LCS(string str1, string str2){
-        // int m = str1.length(), n = str2.length();
-        // int t[m+1][n+1];
-        // for(int i = 0; i < m + 1; i++){
-        //     for( int j = 0; j < n + 1; j++){
-        //         if(i == 0 || j == 0)
-        //             t[i][j] = 0;
-        //         else{
-        //             if(str1[i - 1] == str2[j - 1])
-        //                 t[i][j] = 1 + t[i - 1][j - 1];
-        //             else
-        //                 t[i][j] = max (t[i - 1][j], t[i][j - 1]);
-        //         }
-        //     }
-        // }
-        // int i = m, j = n;
-        // string ans;
-        // while(i>0 && j>0){
-        //     if(str1[i-1]==str2[j-1]){
-        //         ans = ans + str1[i - 1];
-        //         i--;
-        //         j--;
-        //     }
-        //     else if(t[i-1][j] > t[i][j-1])
-        //         i--;
-        //     else j--;
-        // }
-        // reverse(ans.begin(),ans.end());
-        // return ans;
-        int n = str1.size(), m = str2.size();
-        vector<vector<string>> dp(n + 1, vector<string>(m + 1, ""));
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                if (str1[i] == str2[j])
-                    dp[i + 1][j + 1] = dp[i][j] + str1[i];
-                else
-                    dp[i + 1][j + 1] = dp[i + 1][j].size() > dp[i][j + 1].size() ?  dp[i + 1][j] : dp[i][j + 1];
-        return dp[n][m];
+        int m = str1.length(), n = str2.length();
+        int t[m+1][n+1];
+        for(int i = 0; i < m + 1; i++){
+            for( int j = 0; j < n + 1; j++){
+                if(i == 0 || j == 0)
+                    t[i][j] = 0;
+                else{
+                    if(str1[i - 1] == str2[j - 1])
+                        t[i][j] = 1 + t[i - 1][j - 1];
+                    else
+                        t[i][j] = max (t[i - 1][j], t[i][j - 1]);
+                }
+            }
+        }
+        int i = m, j = n;
+        string ans;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                ans = ans + str1[i - 1];
+                i--;
+                j--;
+            }
+            else if(t[i-1][j] > t[i][j-1])
+                i--;
+            else j--;
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+        
     }
     string shortestCommonSupersequence(string str1, string str2) {
         int i = 0, j = 0;
