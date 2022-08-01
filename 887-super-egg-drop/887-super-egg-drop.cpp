4eb6517c0e9182a556b403e100eb2a90049b2739
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-08-01 16:39:56 +0530

    Time: 50 ms (84.42%), Space: 29.5 MB (51.42%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,68 +1,48 @@
 
 class Solution {
 public:
-//     int solve(int e, int n, vector<vector<int>> &t){
-//         if(t[e][n] != -1)
-//             return t[e][n];
+    int solve(int e, int n, vector<vector<int>> &t){
+        if(t[e][n] != -1)
+            return t[e][n];
             
-//         if( e == 1  || n == 0 || n == 1){
-//             return n;
-//         }
-        
-//         int low, high, mn = INT_MAX;
-//         int temp_ans;
-//         for(int k = 1; k <= n; k++ ){
-//             if(t[e - 1][k - 1] != -1)
-//                 low = t[e - 1][k - 1];
-//             else{
-//                 low = solve(e - 1, k - 1, t);
-//                 t[e - 1][k - 1] = low;
-//             }
-//             if(t[e][n - k] != -1)
-//                 high = t[e][n - k];
-//             else{
-//                 high = solve(e, n - k, t);
-//                 t[e][n - k] = high;
-//             }
-            
-//             temp_ans = 1 + max(low, high);
-//             mn = min(mn, temp_ans);
-//         }
-//         return t[e][n] = mn;
-//     }
-    int helper(int K, int N, vector<vector<int>>& memo){
-        if (N <= 1) {
-            return N;
-        }
-        if (K == 1) {
-            return N;
-        }
-        if (memo[K][N] > 0) {
-            return memo[K][N];
+        if( e == 1  || n == 0 || n == 1){
+            return n;
         }
         
-        int low = 1, high = N, result = N;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
-            int left = helper(K - 1, mid - 1, memo);
-            int right = helper(K, N - mid, memo);
-            result = min(result, max(left, right) + 1);
-            if (left == right) {
+        int l = 1, h = n, mn = INT_MAX, mid, low, high;
+        int temp_ans;
+        
+        while (l < h) {
+            mid = l + (h - l) / 2;
+            if(t[e - 1][mid - 1] != -1)
+                low = t[e - 1][mid - 1];
+            else{
+                low = solve(e - 1, mid - 1, t);
+                t[e - 1][mid - 1] = low;
+            }
+            if(t[e][n - mid]!= -1)
+                high = t[e][n - mid];
+            else{
+                high = solve(e, n - mid, t);
+                t[e][n - mid] = high;
+            }
+            mn = min(mn, max(low, high) + 1);
+            if (low == high) {
                 break;
-            } else if (left < right) {
-                low = mid + 1;
+            } else if (low < high) {
+                l = mid + 1;
             } else {
-                high = mid;
+                h = mid;
             }
         }
-        memo[K][N] = result;
-        return result;
+        
+        return t[e][n] = mn;
     }
     //Function to find minimum number of attempts needed in 
     //order to find the critical floor.
     int superEggDrop(int k, int n) {
         vector<vector<int>>t(k+1,vector<int>(n+1,-1));
-        return helper(k, n, t);
+        return solve(k, n, t);
         
     }
 };
