dbe91efd5501e2e83492bea5ad1a5e840456435e
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-08-01 16:42:27 +0530

    Time: 196 ms (25.78%), Space: 29.5 MB (38.53%) - LeetHub

--- 887-super-egg-drop/887-super-egg-drop.cpp
+++ 887-super-egg-drop/887-super-egg-drop.cpp
@@ -1,48 +1,40 @@
-
 class Solution {
 public:
-    int solve(int e, int n, vector<vector<int>> &t){
-        if(t[e][n] != -1)
-            return t[e][n];
-            
-        if( e == 1  || n == 0 || n == 1){
-            return n;
+    int find(int k,int n,vector<vector<int>> &memo)
+    {   if(n==0||n==1) return n;  //if no. of floor 0 , 1 return n:
+        if(k==1) return n;        // if 1 egg return number of floor   
+        if(memo[k][n]!=-1) return memo[k][n];
+        int ans=1000000,l=1,h=n,temp=0;
+     
+        while(l<=h)
+        {
+            int mid=(l+h)/2;
+            int left=find(k-1,mid-1,memo);   //egg broken check for down floors of mid
+            int right=find(k,n-mid,memo) ;   // not broken check for up floors of mid
+            temp=1+max(left,right);          //store max of both 
+            if(left<right){                  //since right is more than left and we need more in worst case 
+              l=mid+1;                       // so l=mid+1 to gain more temp for worst case : upward
+            }
+            else                             //left > right so we will go downward 
+            {    
+                h=mid-1;
+            }
+            ans=min(ans,temp);               //store minimum attempts
         }
         
-        int l = 1, h = n, mn = INT_MAX, mid, low, high;
-        int temp_ans;
-        
-        while (l < h) {
-            mid = l + (h - l) / 2;
-            if(t[e - 1][mid - 1] != -1)
-                low = t[e - 1][mid - 1];
-            else{
-                low = solve(e - 1, mid - 1, t);
-                t[e - 1][mid - 1] = low;
-            }
-            if(t[e][n - mid]!= -1)
-                high = t[e][n - mid];
-            else{
-                high = solve(e, n - mid, t);
-                t[e][n - mid] = high;
-            }
-            mn = min(mn, max(low, high) + 1);
-            if (low == high) {
-                break;
-            } else if (low < high) {
-                l = mid + 1;
-            } else {
-                h = mid;
-            }
-        }
-        
-        return t[e][n] = mn;
+     /*
+        Here we have k eggs and n floor
+        if we drop from i  (i=1 to n):
+         i) egg break , now we remain k-1 eggs and i-1 floor beacase after i floor all the eggs will also break
+        ii) egg not break , now we remain k eggs and n-i floor because before i (included) all eggs will be remain
+      */
+        return memo[k][n]=ans;
+     
     }
-    //Function to find minimum number of attempts needed in 
-    //order to find the critical floor.
-    int superEggDrop(int k, int n) {
-        vector<vector<int>>t(k+1,vector<int>(n+1,-1));
-        return solve(k, n, t);
-        
+    int superEggDrop(int K, int N) {
+        //K -> egg , N -> floor
+        vector<vector<int>> memo(K+1,vector<int> (N+1,-1));
+        return find(K,N,memo);
+         
     }
 };
