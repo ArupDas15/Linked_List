86df092bb6192c629765768f1fd4b4f950a83c0c
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-08-31 01:03:00 +0530

    Time: 15 ms (53.29%), Space: 17.4 MB (54.81%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -14,36 +14,28 @@ class Solution {
 public:
      int widthOfBinaryTree(TreeNode* root) {
        if (!root)
-    return 0;
-  int ans = 0;
-  queue < pair < TreeNode * , unsigned int >> q;
-  q.push({
-    root,
-    0
-  });
-  while (!q.empty()) {
-    int size = q.size();
-    unsigned int curMin = q.front().second;
-    unsigned int leftMost, rightMost;
-    for (int i = 0; i < size; i++) {
-      unsigned int cur_id = q.front().second - curMin; // subtracted to prevent integer overflow
-      TreeNode * temp = q.front().first;
-      q.pop();
-      if (i == 0) leftMost = cur_id;
-      if (i == size - 1) rightMost = cur_id;
-      if (temp -> left)
-        q.push({
-          temp -> left,
-          cur_id * 2 + 1
-        });
-      if (temp -> right)
-        q.push({
-          temp -> right,
-          cur_id * 2 + 2
-        });
-    }
-    if(ans< rightMost - leftMost + 1)ans = rightMost - leftMost + 1;
-  }
-  return ans;
-    }
+           return 0;
+       int ans = 0;
+       queue < pair < TreeNode * , unsigned int >> q;
+       q.push({root,0});
+       while (!q.empty()) {
+            int size = q.size();
+            unsigned int curMin = q.front().second;
+            unsigned int leftMost, rightMost;
+            for (int i = 0; i < size; i++) {
+                unsigned int cur_id = q.front().second - curMin; // subtracted to prevent integer overflow
+                TreeNode * temp = q.front().first;
+                q.pop();
+                if (i == 0) leftMost = cur_id;
+                if (i == size - 1) rightMost = cur_id;
+                if (temp -> left)
+                    q.push({ temp -> left, cur_id * 2 + 1 });
+                if (temp -> right)
+                    q.push({temp -> right, cur_id * 2 + 2});
+            }
+            if(ans< rightMost - leftMost + 1)
+                ans = rightMost - leftMost + 1;
+            }
+            return ans;
+     }
 };
