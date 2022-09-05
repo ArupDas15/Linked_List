c48ad457b6fdb98d65f0641f374caaf452528b52
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-05 14:09:25 +0530

    Time: 8 ms (58.20%), Space: 13.7 MB (32.69%) - LeetHub

---
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+        //queue -> <Key,<Vertical,level>>
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        // Map -> Vertical: map -> level: multiset{Key}>
+        map<int,map<int,multiset<int>>> mp;
+        q.push({root,{0,0}}); //initial vertical and level
+        pair<TreeNode*,pair<int,int>> p;
+        while (q.empty()==false) {
+            p = q.front();
+            q.pop();
+            TreeNode* curr_node = p.first;
+            int vertical = p.second.first;
+            int level = p.second.second;
+            // Insert the key to multiset. By using a multiset we are able to insert multiple duplicate keys into a particular vertical (row) and level (column). 
+            mp[vertical][level].insert(curr_node -> val); 
+            // Push the left child of the curr_node into the queue.
+            if (curr_node -> left) {
+                q.push({curr_node -> left,{vertical - 1, level + 1}});
+            }
+            if (curr_node -> right) {
+                q.push({curr_node -> right,{vertical + 1,level + 1}});
+            }
+        }
+        
+        vector < vector < int >> ans;
+        for (auto p: mp) {
+            vector < int > col;
+            for (auto q: p.second) {
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            }
+            ans.push_back(col);
+        }
+        return ans;
+}
+};
