5acd7e142d9e22b5a6defb1b5b3de398207a60b6
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-07 11:47:22 +0530

    Update 101-symmetric-tree.cpp

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -9,16 +9,23 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Time Complexity: O(n), Space Complexity: O(n)
+// Reference: https://www.youtube.com/watch?v=nKggNAiEpBE&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=26
 class Solution {
 public:
     bool checkSymmetric(TreeNode* leftSubtreeRoot,TreeNode* rightSubtreeRoot){
-        if(leftSubtreeRoot==NULL||rightSubtreeRoot==NULL)return leftSubtreeRoot==rightSubtreeRoot;
+        if(leftSubtreeRoot==NULL||rightSubtreeRoot==NULL)
+            return leftSubtreeRoot==rightSubtreeRoot;
         
-        if(leftSubtreeRoot->val!=rightSubtreeRoot->val) return false;
-        return (checkSymmetric(leftSubtreeRoot->left,rightSubtreeRoot->right)&&checkSymmetric(leftSubtreeRoot->right,rightSubtreeRoot->left));
+        if(leftSubtreeRoot->val!=rightSubtreeRoot->val) 
+            return false;
+        
+        return (checkSymmetric(leftSubtreeRoot->left,rightSubtreeRoot->right) && checkSymmetric(leftSubtreeRoot->right,rightSubtreeRoot->left));
     }
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL)return true;
+        if(root==NULL)
+            return true;
+        
         return checkSymmetric(root->left,root->right);
     }
 };
