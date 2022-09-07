1378b14b5a7f3f3ea4ea4e22bf92ff769bad7316
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-07 11:50:38 +0530

    Update 101-symmetric-tree.cpp

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -10,7 +10,13 @@
  * };
  */
 // Time Complexity: O(n), Space Complexity: O(n)
+
 // Reference: https://www.youtube.com/watch?v=nKggNAiEpBE&list=PLgUwDviBIf0q8Hkd7bK2Bpryj2xVJk8Vk&index=26
+
+// Approach: We will pass the left and right root of the subtree and perform root, left, right traversal for left subtree while simultanouesly performing 
+// root, right, left traversal for the right subtree and checking if the root at both the left and right subtree contain the same value in the node.
+// We are doing the traversal in this way because we want the root node to be the same around the center of the tree (i.e. the root node of the binary tree).
+
 class Solution {
 public:
     bool checkSymmetric(TreeNode* leftSubtreeRoot,TreeNode* rightSubtreeRoot){
