2fc5d51e2b491f348ddf7d389976b01ab954a1c4
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-11 12:13:49 +0530

    Update 94-binary-tree-inorder-traversal.cpp

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -9,12 +9,15 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// Reference: https://www.youtube.com/watch?v=80Zug6D1_r4
+// time Complexity: O(n), Space Complexity: O(1).
 // Based on Morris Traversal. Morris Traversal uses threaded binary tree.
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector < int > inorder;
         TreeNode* curr_node = root;
+        // Time Complexity: O(n) Since we are traversing all the nodes in the given binary tree.
         while(curr_node!=NULL){
             // Case 1: There exists no left subtree for the given node.
             if(curr_node->left==NULL){
@@ -50,3 +53,15 @@ public:
         return inorder;
     }
 };
+
+/*
+Below is my observation regarding Morris Inorder Traversal:
+
+We are visiting the left subtree thrice:
+
+1. To mark the reference to the current node from the rightmost node of the left subtree of the current node.
+2. To perform inorder traversal and store the key in the inoder vector.
+3. To check if there exists a link to the current node, then remove the link and go to the right subtree of the current node.
+
+It is the rightmost subtree in the binary tree, which is visited only once. So we can say the time complexity is O(3n), i.e. O(n).
+*/
