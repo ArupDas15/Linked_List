fce047feecb405b5294e07f2d91ebb80e89f4542
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-11 12:10:34 +0530

    Time: 4 ms (41.16%), Space: 8.5 MB (39.75%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -14,28 +14,39 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector < int > inorder;
-
-  TreeNode * cur = root;
-  while (cur != NULL) {
-    if (cur -> left == NULL) {
-      inorder.push_back(cur -> val);
-      cur = cur -> right;
-    } else {
-      TreeNode * prev = cur -> left;
-      while (prev -> right != NULL && prev -> right != cur) {
-        prev = prev -> right;
-      }
-
-      if (prev -> right == NULL) {
-        prev -> right = cur;
-        cur = cur -> left;
-      } else {
-        prev -> right = NULL;
-        inorder.push_back(cur -> val);
-        cur = cur -> right;
-      }
-    }
-  }
-  return inorder;
+        TreeNode* curr_node = root;
+        while(curr_node!=NULL){
+            // Case 1: There exists no left subtree for the given node.
+            if(curr_node->left==NULL){
+                inorder.push_back(curr_node->val);
+                curr_node=curr_node->right;
+            }
+            else{
+                TreeNode* leftSubtree_node = curr_node->left;
+                // Find the rightmost node of the left subtree and connect it to current
+                // node. Amortised Time complexity: O(n) i.e. even though we run this
+                // while loop for every node in total its time complexity will be O(n).
+                while(leftSubtree_node->right!=NULL && leftSubtree_node->right!=curr_node){
+                    leftSubtree_node=leftSubtree_node->right;
+                }
+                // Case 2: Connect the rightmost node of the left subtree to the current
+                // node.
+                if(leftSubtree_node->right==NULL){
+                    leftSubtree_node->right=curr_node;
+                    curr_node=curr_node->left;
+                }
+                // Case 3: Since the rightmost node of the left subtree is connected to the 
+                // current node, it means that the left subtree is already visited. So we
+                // can push the current node into the inorder vector and move to the right
+                // subtree of the current node.
+                else{
+                    leftSubtree_node->right=NULL;
+                    inorder.push_back(curr_node->val);
+                    curr_node=curr_node->right;
+                }
+            }// end of outer else-construct.
+        }// end of while-loop.
+ 
+        return inorder;
     }
 };
