76295d9e8196062b90ad60179b0d63a8b4a714cc
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-18 22:07:34 +0530

    Update 235-lowest-common-ancestor-of-a-binary-search-tree.cpp

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -11,6 +11,7 @@
 // Time Complexity: O(H) ~ O(n)
 // Space Complexity: O(H) ~ O(n)
 // Reference: https://www.youtube.com/watch?v=cX_kPV_foZc
+// Code to find LCA in a Binary Tree: https://github.com/ArupDas15/Linked_List/blob/master/236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
 
 class Solution {
 public:
