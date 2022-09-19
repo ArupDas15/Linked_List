5f9ec11c62d7cedbe039ece196754fff825e98f0
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-09-19 22:44:57 +0530

    Time: 7 ms (69.92%), Space: 13.8 MB (47.65%) - LeetHub

---
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    // This question is similar to building a binary tree from the preorder traversal and 
+    // inorder traversal, where we can obtain the inorder traversal by just sorting the 
+    // preorder vector(this takes O(nlog n) time) and then we can use the concept of Range 
+    // of a node[Lower Limit, Higher Limit] to construct the tree. This will all together 
+    // take O(n log n) + O(n). We can optimise this further by observing that the Lower 
+    // Limit(root value) is already known to us, we just need to find the upper limit. By 
+    // doing this, in the worst case we are visiting a three times. So our time compleixty 
+    // becomes O(3n) ~ O(n).
+    
+    // Space Complexity: O(n)
+    TreeNode* buildTree(vector<int>&preorder,int &i, int ub){
+        if(i == preorder.size() || preorder[i] > ub)
+            return NULL;
+        TreeNode* root = new TreeNode(preorder[i]);
+        i++;
+        root->left = buildTree(preorder, i, root->val);
+        root->right = buildTree(preorder, i, ub);
+        return root;
+    }
+    TreeNode* bstFromPreorder(vector<int>& preorder) {
+        int i = 0;
+        return buildTree(preorder, i, INT_MAX);
+    }
+};
