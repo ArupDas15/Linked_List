5a053c9430065ac10a2843d4d3111e83714942a6
Arup <37553488+ArupDas15@users.noreply.github.com>
2022-10-13 18:49:54 +0530

    Create Recursively-Printing-Subsequences.cpp

---
+++ Striver_DP/Maximum sum of Non Adjacent Elements/Recursively-Printing-Subsequences.cpp
@@ -0,0 +1,28 @@
+// Reference: https://www.youtube.com/watch?v=AxNNVECce8c
+// Time complexity: O(n*2^n), Space Complexity: O(n)
+#include <bits/stdc++.h>
+#include <set>
+using namespace std;
+void printF(vector<int>a,vector<int>&ds,int idx){
+    if(idx==a.size()){
+        for(int i=0;i<ds.size();i++)
+            cout<<ds[i]<<" ";
+        cout<<endl;
+        return;
+    }
+    // pick the element.
+    ds.push_back(a[idx]);
+    printF(a,ds,idx+1,us);
+    // do not pick the element.
+    ds.pop_back();
+    printF(a,ds,idx+1,us);
+}
+int main() {
+    // Write C++ code here
+    ordered_set<vector<int>>us;
+    vector <int>a={3,1,2};
+    int idx=0;
+    vector<int>ds;
+    printF(a,ds,idx,us);
+    return 0;
+}
