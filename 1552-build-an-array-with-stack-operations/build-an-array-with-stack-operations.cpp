35d866db6cf4f6b7fd7e7f771b66976d1de1e9e6
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-03 09:02:23 +0530

    Update build-an-array-with-stack-operations.cpp

--- 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.cpp
@@ -3,11 +3,12 @@ public:
     // Time Complexity: O(n)
     // Space Complexity: O(n)
     vector<string> buildArray(vector<int>& target, int n) {
-        vector<string> ans;
         int j = 0;
+        vector<string> ans;
+        
         for(int i= 1; i < n+1 && j<target.size(); i++) {
             if(target[j]==i){
-                // this if constraint works because target is trictly increasing
+                // this if constraint works because the target is strictly increasing
                 ans.push_back("Push");
                 j++;
             }else {
