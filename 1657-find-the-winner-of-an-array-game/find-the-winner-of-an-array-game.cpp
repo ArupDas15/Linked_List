15a4afac503b0c9403d05cfd9b897b3c11b3181d
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-05 20:40:20 +0530

    Time: 71 ms (95.83%) | Memory: 63.3 MB (86.11%) - LeetSync

--- 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
     // Time complexity: O(n)
-    // Space complexity: O(n)
+    // Space complexity: O(1)
     int getWinner(vector<int>& arr, int k) {
-        queue<int> q;
-        int winstreak = 0 ;
+        int maxEle;
+        int opponent = 0;     
+        int winstreak = 0;
         int currwinner = 0;
-        int opponent = 0;
-
+        
         currwinner = arr[0];
-        for(int i = 1; i < arr.size(); i++) {
-            q.push(arr[i]);
-        }
-        while(q.empty() == false and winstreak < k){
-            opponent = q.front();
-            q.pop();
-            if (currwinner > opponent) {
-                winstreak++;
-            } else {
-                currwinner = opponent;
-                winstreak = 1;
+        maxEle = *max_element(arr.begin(), arr.end());
+        for(int i = 1; i < arr.size() and 
+            (winstreak < k and currwinner != maxEle); 
+            i++) {
+                opponent = arr[i];
+                if (currwinner > opponent) {
+                    winstreak++;
+                } else {
+                    currwinner = opponent;
+                    winstreak = 1;
+                }
             }
-        }
         return currwinner;
     }
 };
+// class Solution {
+// public:
+//     // Time complexity: O(n)
+//     // Space complexity: O(n)
+//     int getWinner(vector<int>& arr, int k) {
+//         queue<int> q;
+//         int winstreak = 0 ;
+//         int currwinner = 0;
+//         int opponent = 0;
+
+//         currwinner = arr[0];
+//         for(int i = 1; i < arr.size(); i++) {
+//             q.push(arr[i]);
+//         }
+//         while(q.empty() == false and winstreak < k){
+//             opponent = q.front();
+//             q.pop();
+//             if (currwinner > opponent) {
+//                 winstreak++;
+//             } else {
+//                 currwinner = opponent;
+//                 winstreak = 1;
+//             }
+//         }
+//         return currwinner;
+//     }
+// };
