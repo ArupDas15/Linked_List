2336904223c04750d29b07e4ee6db3c444dcc27e
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-05 21:44:23 +0530

    Update last-moment-before-all-ants-fall-out-of-a-plank.cpp

--- 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
+++ 1627-last-moment-before-all-ants-fall-out-of-a-plank/last-moment-before-all-ants-fall-out-of-a-plank.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
-    int getLastMoment(int n, vector<int>& left, vector<int>& right) {
+       // Time Complexity: O(n)
+int getLastMoment(int n, vector<int>& left, vector<int>& right) {
         int ans = 0;
         for (int num : left) {
             ans = max(ans, num);
