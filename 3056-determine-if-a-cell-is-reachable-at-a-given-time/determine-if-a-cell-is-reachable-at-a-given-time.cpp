127ce500f052250ce26a98ef7878077136a2b532
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-08 19:31:08 +0530

    Update determine-if-a-cell-is-reachable-at-a-given-time.cpp

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -1,9 +1,19 @@
 class Solution {
 public:
 /*
-Practically, on a grid, Chebyshev distance represents distance measured as if the shortest path between two points can take steps in any of the 8 grid directions at equal cost. Thus, by definition, Chebyshev distance is generally a useful distance measurement in games that allow unrestricted 8-way movement, where moving diagonally costs no more than moving in a cardinal direction.
+In a 2D grid, the Chebyshev distance represents the shortest path between
+two points provided, one can take steps in any of the 8 grid directions at
+equal cost. Thus, by definition, Chebyshev distance is generally a useful
+distance measurement in games that allow unrestricted 8-way movement
+where moving diagonally costs no more than moving in a cardinal direction.
 */
+    // Time complexity: O(1)
+    // Space complexity: O(1)
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        /* We cannot move to an adjacent point in one unit of time but 
+        we cannot come back to the start/finish time. If start and end 
+        refer to the same cell, we can successfully move from start to 
+        end in given time t if t != 1.*/
         if(sx==fx and sy==fy and t==1) {
             return false;
         } 
