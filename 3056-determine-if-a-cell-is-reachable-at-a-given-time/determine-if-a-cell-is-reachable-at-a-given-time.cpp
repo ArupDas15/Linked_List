aea89f2031231dd49ce487d8c3ac50a80d374ba9
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-08 19:21:46 +0530

    Time: 2 ms (36.53%) | Memory: 6.3 MB (10.74%) - LeetSync

--- 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
+++ 3056-determine-if-a-cell-is-reachable-at-a-given-time/determine-if-a-cell-is-reachable-at-a-given-time.cpp
@@ -4,9 +4,9 @@ public:
 Practically, on a grid, Chebyshev distance represents distance measured as if the shortest path between two points can take steps in any of the 8 grid directions at equal cost. Thus, by definition, Chebyshev distance is generally a useful distance measurement in games that allow unrestricted 8-way movement, where moving diagonally costs no more than moving in a cardinal direction.
 */
     bool isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
-        if((sx==fx and sy==fy) and t==1)return false;
-        // cout<<fx-sx<<endl<<fy-sy<<endl<<max(fx-sx,fy-sy);
-        if (max(abs(fx-sx),abs(fy-sy)) <= t) return true;
-        return false;
+        if(sx==fx and sy==fy and t==1) {
+            return false;
+        } 
+        return max(abs(fx-sx),abs(fy-sy)) <= t;
     }
 };
