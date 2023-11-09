811fa51a4c4b8b2d95c8098f7e95fccfa5fc5070
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-09 08:47:42 +0530

    Update count-number-of-homogenous-substrings.cpp

--- 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
+++ 1885-count-number-of-homogenous-substrings/count-number-of-homogenous-substrings.cpp
@@ -1,5 +1,10 @@
 class Solution {
 public:
+    // Time Complexity: O(n)
+    // Space Complexity: O(1)
+    // Intuition: For a character at index 'i', the total number of ways a string
+    // can end at that index is (i + 1). We are accounting for 1 by initialising
+    // ans with s.size(). 
     int countHomogenous(string s) {
         int MOD = 1000000007;
         int ans = (int) s.size();
