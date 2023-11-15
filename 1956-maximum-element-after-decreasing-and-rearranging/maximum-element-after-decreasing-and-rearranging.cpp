f5ef442a65894984f74796099ffcd19479ed51e1
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-15 07:41:47 +0530

    Time: 82 ms (40.67%) | Memory: 53.6 MB (5.22%) - LeetSync

--- 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
+    // Time complexity: O(n)
+    // Space complexity: O(n)
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        int ans = 1;
+        int n = arr.size();
+        vector<int> counts(n + 1, 0);
+
+        for(int num : arr) {
+            counts[min(num,n)]++;
+        }
+        for(int num = 2; num <= n; num++) {
+            ans = min(ans + counts[num], num);
+        }
+        return ans;
+    }
+};
+
+// class Solution {
+// public:
     // Time complexity: O(n⋅log⁡n)
     // Space complexity: O(log⁡n)
     /* ----------------------------------------------------------------------------
     In C++, the sort() function provided by STL uses a hybrid of Quick Sort, 
     Heap Sort and Insertion Sort, with a worst-case space complexity of O(log⁡n).
     -------------------------------------------------------------------------------*/
-    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
-        sort(arr.begin(),arr.end());
-        arr[0] = 1;
-        for(int i = 1; i < arr.size(); i++) {
-            if(abs(arr[i] - arr[i - 1]) <= 1) {
-                continue;
-            } else {
-                arr[i] = arr[i - 1] + 1;
-            }
-        }
-        return arr[arr.size() - 1];
-    }
-};
+//     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+//         sort(arr.begin(),arr.end());
+//         arr[0] = 1;
+//         for(int i = 1; i < arr.size(); i++) {
+//             if(abs(arr[i] - arr[i - 1]) <= 1) {
+//                 continue;
+//             } else {
+//                 arr[i] = arr[i - 1] + 1;
+//             }
+//         }
+//         return arr[arr.size() - 1];
+//     }
+// };
