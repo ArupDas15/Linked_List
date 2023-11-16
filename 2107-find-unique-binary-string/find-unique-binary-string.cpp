222d8246016d4f5e5f44980de4f6ba061e55ae7b
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-16 23:39:35 +0530

    Time: 3 ms (74.62%) | Memory: 10.5 MB (86.71%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,5 +1,26 @@
 class Solution {
 public:
+    // Cantor's Diagonal Argument: https://en.wikipedia.org/wiki/Cantor%27s_diagonal_argument
+    // Time complexity: O(n)
+    // Space complexity: O(1)
+    string findDifferentBinaryString(vector<string>& nums) {
+        string curr = "";
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i][i] == '0') {
+                curr = curr + "1";
+            } else {
+                curr = curr + "0";
+            }
+        }
+        return curr;
+    }
+};
+
+/*
+// Time complexity: O(n^2)
+// space complexity: O(n)
+class Solution {
+public:
     set<string>numset;
     string generate(string curr) {
         if(curr.length() == numset.size()) {
@@ -21,3 +42,4 @@ public:
         return generate("");
     }
 };
+*/
