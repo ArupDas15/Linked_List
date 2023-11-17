627079ea0d7b1e28da495dc159e10e6a45894d23
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-18 00:05:53 +0530

    Update minimize-maximum-pair-sum-in-array.cpp

--- 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ 1988-minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
+    // Time Complexity: O(nlogn)
+    // Space Complexity: O(logn)
     int minPairSum(vector<int>& nums) {
         int ans = INT_MIN;
-        
+
         sort(nums.begin(),nums.end());
-        for(int i = 0, j = nums.size() - 1; i < nums.size()/2 && j >= nums.size()/2; i++, j--) {
+        for(int i = 0, j = nums.size() - 1; 
+            i < nums.size()/2 && j >= nums.size()/2; 
+            i++, j--) {
             ans = max(ans,nums[i] + nums[j]);
         }
         return ans;
