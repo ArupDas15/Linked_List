87df11e8c9ee224ab33e18dd53ee5e731248886e
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-19 10:39:07 +0530

    Update reduction-operations-to-make-the-array-elements-equal.cpp

--- 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
+++ 2016-reduction-operations-to-make-the-array-elements-equal/reduction-operations-to-make-the-array-elements-equal.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
+    // time complexity: O(nlogn)
+    // space complexity: O(logn)
     int reductionOperations(vector<int>& nums) {
-        int ans = 0, up = 0;
+        int ans = 0;
 
         sort(nums.begin(), nums.end());        
         for(int i = 1; i < nums.size(); i++) {
             if(nums[i] != nums[i-1]) {
-                up++;
+                ans = ans + (nums.size() - i);
             }
-            ans = ans + up;
         }
         return ans;
     }
