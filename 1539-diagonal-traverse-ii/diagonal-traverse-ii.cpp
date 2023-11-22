2eb95546b89919f91528ceb810de0865bb344563
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-22 18:29:07 +0530

    Time: 493 ms (5.12%) | Memory: 437.5 MB (5.13%) - LeetSync

---
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    // Time Complexity: O(nm)
+    // Space Complexity: O(n)
+    // Whenever asked to do diagonal traversal in a tree or diagonal 
+    // matrix etc. think of a solution involving a map.
+    
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        map<int, stack<int>> mp;
+        vector<int> ans;
+
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = 0; j < nums[i].size(); j++) {
+                mp[i + j].push(nums[i][j]);
+            }
+        }
+        for(auto it : mp) {
+            int key = it.first;
+            while(!mp[key].empty()) {
+                ans.push_back(mp[key].top());
+                mp[key].pop();
+            }
+        }
+        return ans;
+    }
+};
