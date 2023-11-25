6ca47bb885c01550c52b9171249fb4032d991d1f
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-25 08:57:15 +0530

    Time: 79 ms (94.32%) | Memory: 87.7 MB (40.81%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     // time complexity: O(n)
-    // space complexity: O(n)
+    // space complexity: O(1)
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
-        int n = nums.size();
         vector<int> ans;
-        vector<int>prefixsum(n,0);
+        int leftsum = 0;
+        int n = nums.size();
+        int totalsum = accumulate(nums.begin(),nums.end(),0);
 
-        prefixsum[0] = nums[0];
-        for(int i = 1; i < n; i++) {
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-        }
         for(int i = 0; i < n; i++) {
             int leftcount = i;
             int rightcount = n - 1 - i;
-            int left_half = leftcount > 0 ?
-                                (leftcount*nums[i])-prefixsum[i-1]:0;
-            int right_half = (prefixsum[n-1]-prefixsum[i])-
-                                (rightcount*nums[i]);
+            int rightsum = totalsum - leftsum - nums[i];
+            int left_half = (leftcount * nums[i]) - leftsum;
+            int right_half = rightsum - (rightcount * nums[i]);
+
             ans.push_back(left_half + right_half);
+            leftsum = leftsum + nums[i];
         }
         return ans;
     }
