76ba9ba53c47f0a8c8163837693eb2201e7072bd
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-25 09:04:17 +0530

    Update sum-of-absolute-differences-in-a-sorted-array.cpp

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -34,7 +34,7 @@ public:
 
 //         prefixsum[0] = nums[0];
 //         for(int i = 1; i < n; i++) {
-//             prefixsum[i] = prefixsum[i - 1]+nums[i];
+//             prefixsum[i] = prefixsum[i - 1] + nums[i];
 //         }
 //         for(int i = 0; i < n; i++) {
 //             int leftcount = i;
