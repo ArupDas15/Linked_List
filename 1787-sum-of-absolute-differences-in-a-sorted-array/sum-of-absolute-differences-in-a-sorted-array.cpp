2a17bb4e053edeac1514472cf693d6fdde68270b
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-11-25 08:59:53 +0530

    Time: 94 ms (53.51%) | Memory: 87.7 MB (40.81%) - LeetSync

--- 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
+++ 1787-sum-of-absolute-differences-in-a-sorted-array/sum-of-absolute-differences-in-a-sorted-array.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // Approach 2: Calculating prefix sum on the fly
     // time complexity: O(n)
     // space complexity: O(1)
     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
@@ -21,3 +22,29 @@ public:
         return ans;
     }
 };
+// Approach 1: Pre-computing prefix sum
+// class Solution {
+// public:
+    // time complexity: O(n)
+    // space complexity: O(n)
+//     vector<int> getSumAbsoluteDifferences(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<int> ans;
+//         vector<int>prefixsum(n,0);
+
+//         prefixsum[0] = nums[0];
+//         for(int i = 1; i < n; i++) {
+//             prefixsum[i]=prefixsum[i-1]+nums[i];
+//         }
+//         for(int i = 0; i < n; i++) {
+//             int leftcount = i;
+//             int rightcount = n - 1 - i;
+//             int left_half = leftcount > 0 ?
+//                                 (leftcount*nums[i])-prefixsum[i-1]:0;
+//             int right_half = (prefixsum[n-1]-prefixsum[i])-
+//                                 (rightcount*nums[i]);
+//             ans.push_back(left_half + right_half);
+//         }
+//         return ans;
+//     }
+// };
