715bcbcecdfea50a71bdf827d54e66fc4442d847
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-01 06:23:35 +0530

    Time: 4 ms (57.15%) | Memory: 11.6 MB (95.20%) - LeetSync

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
-    // Time Complexity: O(nk) where k is the maximum length of the string in the string vector.
-    // Space Complexity: O(nk)
+    // Time Complexity: O(n) where k is the maximum length of the string in the string vector.
+    // Space Complexity: O(1)
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
-        string s1 = "", s2 = "";
+        int wrd1Ptr, wrd2Ptr, str1Ptr, str2Ptr;
 
-        for(string s : word1) {
-            s1 = s1 + s;
+        wrd1Ptr = wrd2Ptr = str1Ptr = str2Ptr = 0;
+        while (wrd1Ptr < word1.size() && wrd2Ptr < word2.size()) {
+            if (word1[wrd1Ptr][str1Ptr++] != word2[wrd2Ptr][str2Ptr++]) {
+                return false;
+            }
+            if (str1Ptr == word1[wrd1Ptr].size()) {
+                wrd1Ptr++;
+                str1Ptr = 0;
+            }
+            if (str2Ptr == word2[wrd2Ptr].size()) {
+                wrd2Ptr++;
+                str2Ptr = 0;
+            } 
         }
-        for(string s : word2) {
-            s2 = s2 + s;
-        }
-        return s1 == s2;
+        // checking if both string vectors have been traversed completely.
+        return (wrd1Ptr == word1.size()) && (wrd2Ptr == word2.size());
     }
 };
