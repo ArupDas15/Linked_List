9a888e7e4461f85e609af7bb959056945294f8cf
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-01 06:24:35 +0530

    Update check-if-two-string-arrays-are-equivalent.cpp

--- 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
+++ 1781-check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -23,3 +23,20 @@ public:
         return (wrd1Ptr == word1.size()) && (wrd2Ptr == word2.size());
     }
 };
+
+// class Solution {
+// public:
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+//     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+//         string s1 = "", s2 = "";
+
+//         for(string s : word1) {
+//             s1 = s1 + s;
+//         }
+//         for(string s : word2) {
+//             s2 = s2 + s;
+//         }
+//         return s1 == s2;
+//     }
+// };
