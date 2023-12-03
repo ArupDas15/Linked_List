e980bb843709be40b16533b43610ec930dbedf05
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-03 23:08:06 +0530

    Time: 14 ms (6.23%) | Memory: 10.5 MB (64.64%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    // time complexity: O(nk) where k is the maximum distance between 2 points.
+    // space complexity: O(1)
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        if (points.size() == 1) return 0;
+        
+        int steps;
+        int prev_x1 = points[0][0];
+        int prev_y1 = points[0][1];
+
+        for(int i = 1; i < points.size(); i++) {
+            int cur_x1 = points[i][0];
+            int cur_y1 = points[i][1];
+
+            while(prev_x1 != cur_x1 || prev_y1 != cur_y1) {
+                if (prev_x1 > cur_x1) 
+                    prev_x1 = prev_x1 - 1;
+                else if (prev_x1 < cur_x1) 
+                    prev_x1 = prev_x1 + 1;
+                if (prev_y1 > cur_y1) 
+                    prev_y1 = prev_y1 - 1;
+                else if (prev_y1 < cur_y1) 
+                    prev_y1 = prev_y1 + 1;
+                steps = steps + 1;
+            }
+        }
+        return steps;
+    }
+};
