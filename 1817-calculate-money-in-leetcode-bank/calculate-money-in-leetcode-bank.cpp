e4c6cbfef27cbc9388a29d55f688919f53f02dcd
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-06 08:01:58 +0530

    Update calculate-money-in-leetcode-bank.cpp

--- 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
+    // Time complexity: O(n)
+    // Space complexity: O(1)
     int totalMoney(int n) {
-        int bal;
+        int bal = 0;
         int total = 0;
         int monday = 0;
 
+        // while-loop iterates n/7 times.
         while(n > 0) {
             if(n < 7) {
               bal = n;
@@ -15,6 +18,12 @@ public:
               n = n - 7;
             }
             monday += 1;
+            // Sum on n terms in an AP where d = 1.
+            // Sn = n/2*(2a+(n-1)d)
+            // As per the constraints, 1 <= n <= 1000
+            // that is why we modified Sn = (n*(2a+(n-1)d))/2
+            // otherwise we would need to convert Sn to 
+            // floating type and return an integer.
             total += (bal*((2*monday) + (bal - 1)))/2;
         }    
         return total;
