2c1987e49e4dd5bd8211bece419e880bb795686a
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-07 07:30:41 +0530

    Time: 31 ms (43.14%) | Memory: 15.2 MB (58.43%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -3,14 +3,11 @@ public:
     // Time complexity: O(n)
     // Space complexity: O(1) 
     string largestOddNumber(string num) {
-        int largest_odd_pos = -1;
-
-        for(int i = num.length() - 1; i >= 0; i--) {
-            if((num[i]-'0') % 2 != 0) {
-                largest_odd_pos = i;
-                break;
-            }
-        }
-        return largest_odd_pos == -1 ? "" : num.substr(0, largest_odd_pos + 1); 
+      for(int i = num.length() - 1; i >= 0; i--) {
+          if((num[i]-'0') % 2 != 0) {
+            return num.substr(0, i + 1);
+          }
+      }
+      return ""; 
     }
 };
