ecb75fd7b7b48e7f46ae73dec64305b89422d5a9
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-07 07:35:03 +0530

    Time: 27 ms (73.43%) | Memory: 15.2 MB (76.98%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -4,7 +4,11 @@ public:
     // Space complexity: O(1) 
     string largestOddNumber(string num) {
       for(int i = num.length() - 1; i >= 0; i--) {
-          if((num[i]-'0') % 2 != 0) {
+          // NOTE: the ASCII values for characters with even values are
+          // themselves even, and odd for characters that represent odd 
+          // digits. So no need to do (num[i] - '0') % 2 != 0 to determine
+          // if the ith digit in num is an odd digit.
+          if((num[i]) % 2 != 0) {
             return num.substr(0, i + 1);
           }
       }
