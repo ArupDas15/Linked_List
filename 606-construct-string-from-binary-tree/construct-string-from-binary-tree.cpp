c65aeda73871a77742ae5ba53342ac7a3c7a9db0
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-08 06:55:54 +0530

    Time: 16 ms (55.74%) | Memory: 24 MB (70.39%) - LeetSync

--- 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/construct-string-from-binary-tree.cpp
@@ -10,38 +10,45 @@
  * };
  */
 class Solution {
+private:
+    string ans = "";
 public:
-    void printPreorder(TreeNode* node,string& ans) {
+    
+    void printPreorder(TreeNode* node) {
         if (node == nullptr) {
             ans.append("()");
             return;
         } else ans.append(to_string(node->val));
 
         if(node->left != nullptr and node->right == nullptr) {
+            // if left subtree exists and right subtree does not - ignore 
+            // right subtree
             ans.append("(");
             // Recur on left subtree
-            printPreorder(node->left, ans);
+            printPreorder(node->left);
             ans.append(")");
         } else if(node->left == nullptr and node->right != nullptr) {
+            // if left subtree does not exists and right subtree does -
+            // do not ignore left subtree
             ans.append("()(");
             // Recur on right subtree
-            printPreorder(node->right, ans);
+            printPreorder(node->right);
             ans.append(")");
         } else if (node->left != nullptr and node->right != nullptr) {
+            // if left and right subtree exists - recurse on both subtrees
             ans.append("(");
             // Recur on left subtree
-            printPreorder(node->left, ans);
+            printPreorder(node->left);
             ans.append(")");
             ans.append("(");
             // Recur on right subtree
-            printPreorder(node->right, ans);
+            printPreorder(node->right);
             ans.append(")");
         }
     }
 
     string tree2str(TreeNode* root) {
-        string ans = "";
-        printPreorder(root, ans);
+        printPreorder(root);
         return ans;
     }
 };
