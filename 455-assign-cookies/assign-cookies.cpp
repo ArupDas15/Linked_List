f4c8d89734a8ec58d691005c4b572d2b2baffe75
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-15 16:44:19 +0530

    Update assign-cookies.cpp

--- 455-assign-cookies/assign-cookies.cpp
+++ 455-assign-cookies/assign-cookies.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    // Time complexity: O(N log N + M log M)
+    // Space complexity: O(log N + log M)
     int findContentChildren(vector<int>& g, vector<int>& s) {
         int ans = 0;
         int j = g.size() - 1;
