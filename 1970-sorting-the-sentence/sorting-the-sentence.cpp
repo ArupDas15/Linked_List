51e20a72dfc9d31930fb372e48d04a9d91a5c572
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2023-12-30 14:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (40.88%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string sortSentence(string s) {
+        string arr[9];
+        string temp = "";
+        
+        for(int i = 0; i < 9; i++) {
+            arr[i] = "";
+        }
+        for(char c : s) {
+            if(c != ' ' ) {
+                if(isdigit(c)) {
+                    arr[c - '0' - 1] = temp;
+                    temp = "";
+                } else {
+                    temp.push_back(c);
+                }
+            }
+        }
+        temp = "";
+        for(int i = 0; i < 9; i++) {
+            if(arr[i] != "") {
+                temp = temp + arr[i];
+                if(i + 1 < 9 and arr[i + 1] != "") {
+                    temp = temp + " ";
+                }
+            }
+        }
+        return temp;
+    }
+};
