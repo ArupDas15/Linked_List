f98dd2751442a7de5103fc73db32d2fd163cb5b8
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-01-10 08:21:50 +0530

    Time: 492 ms (33.04%) | Memory: 146.5 MB (84.01%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+int burnTime(map <TreeNode*,TreeNode*> &omp,TreeNode* start){
+    queue<TreeNode*> q;
+    map <TreeNode*, int>vis;
+    int timer=0;
+    q.push(start);
+    vis[start]=1;
+    TreeNode* curr_node;
+    bool flg;
+    while(q.empty()==false){
+        int count = q.size();
+        flg=false;
+        for(int i=0;i<count;i++){
+            curr_node=q.front();
+            q.pop();
+            if(curr_node->left!=NULL && !vis[curr_node->left]){
+                q.push(curr_node->left);
+                vis[curr_node->left]=1;
+                flg=true;
+            }
+            if(curr_node->right!=NULL && !vis[curr_node->right]){
+                q.push(curr_node->right);
+                vis[curr_node->right]=1;
+                flg=true;
+            }
+            if( omp[curr_node] && !vis[omp[curr_node]]){
+                q.push(omp[curr_node]);
+                vis[omp[curr_node]]=1;
+                flg=true;
+            }
+        }
+        if(flg==true){
+            timer++;
+        }
+    }
+    return timer;
+}
+
+TreeNode* MarkParentbyBFS(TreeNode* root, int start,map <TreeNode*,TreeNode*>&omp){
+    queue<TreeNode*> q;
+    TreeNode* res;
+    q.push(root);
+    TreeNode* curr_node;
+    while(q.empty()==false){
+        curr_node=q.front();
+        q.pop();
+        if(curr_node->val==start){
+            res=curr_node;
+        }
+        if(curr_node->left!=NULL){
+                //Mark current node -> parent node
+                omp[curr_node->left]=curr_node;
+                q.push(curr_node->left);
+            }
+            if(curr_node->right!=NULL){
+                omp[curr_node->right]=curr_node;
+                q.push(curr_node->right);
+            }
+        }
+        return res;   
+}
+
+class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        map <TreeNode*,TreeNode*> omp;
+        TreeNode*start_node=MarkParentbyBFS(root,start,omp);
+        return burnTime(omp,start_node);
+    }
+};
