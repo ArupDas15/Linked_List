e8e73560d10e86ee66a9bc3d81f6431214c6231e
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-01-26 15:12:05 +0530

    Time: 280 ms (24.31%) | Memory: 122.8 MB (7.29%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) 
+    {
+        map<int,int>m;
+        for(int i=0;i<candyType.size();i++)
+        {
+            m[candyType[i]]++;
+        }
+        int eat = candyType.size()/2;
+        //return eat;
+        int type = m.size();
+        cout<<type<<endl;
+        if(eat>=type)
+        {
+            return type;
+        }
+        else
+        {
+            return eat;
+        }
+    }
+};
