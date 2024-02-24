0c0688b8d48db3b895e492971709cd283527b430
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-02-25 01:12:26 +0530

    Time: 96 ms (14.90%) | Memory: 70.2 MB (20.43%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,26 +1,32 @@
 class Solution {
 public:
+    int maxCrossSubArraySum(vector<int>& nums, int l, int r, int mid) {
+        int temp_sum = 0;
+        int cross_sum = 0;
+        int max_left_sub_sum = INT_MIN;
+        int max_right_sub_sum = INT_MIN;
+
+        for(int i = mid; i >= l; i--) {
+            temp_sum += nums[i];
+            max_left_sub_sum = max(max_left_sub_sum, temp_sum);
+        }
+        temp_sum = 0;
+        for(int i = mid + 1; i <= r; i++) {
+            temp_sum += nums[i];
+            max_right_sub_sum = max(max_right_sub_sum, temp_sum);
+        }
+        cross_sum = max_left_sub_sum + max_right_sub_sum;
+        return cross_sum;
+    }
+    int maxSubArraySum(vector<int>& nums, int l, int r) {
+        if(l == r) return nums[l];
+        int mid = l + ((r - l) / 2);
+        int left_sum =  maxSubArraySum(nums, l, mid);
+        int right_sum = maxSubArraySum(nums, mid + 1, r);
+        int cross_sum = maxCrossSubArraySum(nums, l, r, mid);
+        return (left_sum > right_sum)? ((left_sum > cross_sum) ? left_sum : cross_sum) : ((right_sum > cross_sum) ? right_sum : cross_sum);
+    }
     int maxSubArray(vector<int>& nums) {
-        int maxi = INT_MIN;
-        int n = nums.size();
-        int sum = 0;
-        bool negflg = true;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > 0) {
-                negflg = false;
-                break;
-            } else {
-                maxi = max(maxi, nums[i]);
-            }
-        }
-        if(negflg) return maxi;
-
-        for(int i = 0; i < n; i++) {
-            sum = sum + nums[i];
-            if(sum < 0) sum = 0;
-            maxi = max(maxi, sum);
-        }
-        return maxi;
+        return maxSubArraySum(nums, 0, nums.size() - 1);
     }
 };
