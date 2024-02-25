d83304bfcf8812c55ed72dd4ae4589842fce78f8
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-02-25 11:05:35 +0530

    Time: 23 ms (76.20%) | Memory: 22.8 MB (27.79%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -6,18 +6,17 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         vector<int> temp_vec;
-        bool last_vec = false;
+        bool last_vec_processed = false;
         
         sort(intervals.begin(), intervals.end(), sortcol);
-        for(int i = 0; i < intervals.size() and !last_vec; i++) {
+        for(int i = 0; i < intervals.size() and !last_vec_processed; i++) {
             temp_vec.push_back(intervals[i][0]);
             int new_end = intervals[i][1];
             for(int j = i + 1; j < intervals.size(); j++) {
                 if(intervals[j][0] <= new_end) {
                     new_end = max(new_end, intervals[j][1]);
                     if (j == intervals.size() - 1) {
-                        last_vec = true;
-                        cout<<"True";
+                        last_vec_processed = true;
                     }
                     continue;
                 } else {
