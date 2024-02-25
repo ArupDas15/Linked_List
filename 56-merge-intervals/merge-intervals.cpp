8f60342bb47c3e837ac1c317453dddf02f5771d0
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-02-25 11:32:56 +0530

    Time: 22 ms (80.59%) | Memory: 22.7 MB (43.06%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,32 +2,13 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
-        vector<int> temp_vec;
-        bool last_vec_processed = false;
         
         sort(intervals.begin(), intervals.end());
-        for(int i = 0; i < intervals.size() and !last_vec_processed; i++) {
-            temp_vec.push_back(intervals[i][0]);
-            int new_end = intervals[i][1];
-            for(int j = i + 1; j < intervals.size(); j++) {
-                if(intervals[j][0] <= new_end) {
-                    new_end = max(new_end, intervals[j][1]);
-                    if (j == intervals.size() - 1) {
-                        last_vec_processed = true;
-                    }
-                    continue;
-                } else {
-                    temp_vec.push_back(new_end);
-                    ans.push_back(temp_vec);
-                    temp_vec.clear();
-                    i = j - 1;
-                    break;
-                }
-            }
-            if(temp_vec.size() == 1) {
-                temp_vec.push_back(new_end);
-                ans.push_back(temp_vec);
-                temp_vec.clear();
+        for(int i = 0; i < intervals.size(); i++) {
+            if(ans.empty() || ans.back()[1] < intervals[i][0]) {
+                ans.push_back(intervals[i]);
+            } else {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
         }
         return ans;
