0b9c336032c4937bab92ae00f5f1d43be898b6d5
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-02-25 17:20:59 +0530

    Time: 5 ms (29.74%) | Memory: 11 MB (19.51%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left;
-        int right;
-
-        left = m - 1;
-        right = 0;
-        while(left >= 0 && right < n) {
-            if(nums1[left] > nums2[right]) {
-                swap(nums1[left], nums2[right]);
-                left--;
-                right++;
-            } else if(nums1[left] <= nums2[right]) {
-                break;
-            }
-        }
-        sort(nums1.begin(), nums1.begin() + m);
-        sort(nums2.begin(), nums2.end());
         for(int i = m, j = 0; i< m + n && j < n; i++, j++) {
             nums1[i] = nums2[j];
         }
+        
+        int gap = ceil(floor(m + n) / 2);
+        int left = 0;
+        int right = left + gap;
+        
+        for(;gap > 0; gap = ceil(floor(gap) / 2)) {
+            left = 0;
+            right = left + gap;
+            while(right < nums1.size() ) {
+                if(nums1[left] > nums1[right]) {
+                    swap(nums1[left], nums1[right]);
+                }
+                left++;
+                right++;
+            }
+            if(gap == 1) {
+                    break;
+                }
+        }
+        
     }
 };
