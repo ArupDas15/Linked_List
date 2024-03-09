80d5cba17b4dc129f81f3e9f58fff16e678c24c7
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-03-09 20:37:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (70.72%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,13 +1,36 @@
 class Solution {
 public:
-    int countPaths(int m, int n, int i, int j, vector<vector<int>>& dp) {
-        if(i >= m || j >= n) return 0;
-        else if(i == m - 1 && j == n - 1) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = countPaths(m, n, i + 1, j, dp) + countPaths(m, n, i, j + 1, dp);
+    // Intuition: You will move n-1 stepa in the right, m-1 steps downwards
+    // to reach m-1, n-1. So, total steps taken is (m-1) + (n-1) = m+n-2
+    // Out of m+n-2 steps, if we choose m-1 downwars steps remaining steps
+    // will be in rightward direction and vice-versa.
+
+    // Time complexity: min(O(m-1), O(n-1))
+    // Space Complexity: O(1)
+    int comb(int n, int m) {
+        double numerator = 1, denominator = 1;
+
+        for(int i = 1; i <= m; i++) {
+            numerator = numerator * n;
+            denominator = denominator * i;
+            n = n - 1;
+        }
+        return (int) (numerator/denominator);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        return countPaths(m, n, 0, 0, dp);
+
+        return comb(m + n - 2, min(m-1, n-1));
     }
+    // Time Complexity: O(m*n)
+    // Space Complexity: O(m*n)
+    // int countPaths(int m, int n, int i, int j, vector<vector<int>>& dp) {
+    //     if(i >= m || j >= n) return 0;
+    //     else if(i == m - 1 && j == n - 1) return 1;
+    //     if(dp[i][j] != -1) return dp[i][j];
+    //     return dp[i][j] = countPaths(m, n, i + 1, j, dp) + countPaths(m, n, i, j + 1, dp);
+    // }
+    // int uniquePaths(int m, int n) {
+    //     vector<vector<int>> dp(m, vector<int>(n, -1));
+    //     return countPaths(m, n, 0, 0, dp);
+    // }
 };
