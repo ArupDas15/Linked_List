548df9d1fd6a537be8e0381268e87b345e645d13
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-03-09 16:32:25 +0530

    Time: 51 ms (98.19%) | Memory: 53.1 MB (29.59%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.cpp
+++ 2634-minimum-common-value/minimum-common-value.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
-    int two_ptr_srch(vector<int>& nums1, vector<int>& nums2) {
-        int j = 0;
-        for(int i = 0; i < nums1.size() && j < nums2.size();) {
-            if(nums1[i] == nums2[j]) return nums1[i];
-            else if(nums1[i] < nums2[j]) i++;
-            else j++;
-        }
-        return -1;
-    }
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-       if(nums1.size() < nums2.size()) {
-         return two_ptr_srch(nums1, nums2);
+        if (nums1.size() + nums2.size() > min(nums1.size(), nums2.size())*log (max(nums1.size(), nums2.size()))) {
+            if(nums1.size() < nums2.size()) {
+         for(int x : nums1) {
+             if(binary_search(nums2.begin(), nums2.end(), x)) return x;
+         }  
        } 
-       return two_ptr_srch(nums2, nums1);
+       for(int x : nums2) {
+           if(binary_search(nums1.begin(), nums1.end(), x)) return x;
+       }
+       return -1;
+        }
+        else {
+            int i = 0;
+            int j = 0;
+            while(i < nums1.size() && j < nums2.size()) {
+                if(nums1[i] == nums2[j]) return nums1[i];
+                else if(nums1[i] < nums2[j]) i++;
+                else j++;
+            }
+            return -1;
+        }
+
+        
     }
 };
