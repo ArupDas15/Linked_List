26927b7fd6da0bdf47a7af9562ab345d0436e39e
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-03-10 20:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (50.55%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -6,11 +6,10 @@ public:
         string ans = "";
         int n = strs.size() - 1;
         int n1 = strs[0].length();
-        int n2 = strs[n].length();
         
         sort(strs.begin(), strs.end());
-        for(int i = 0, j = 0; i < n1 && j < n2; i++, j++) {
-            if(strs[0][i] == strs[n][j]) ans.push_back(strs[0][i]);
+        for(int i = 0; i < n1; i++) {
+            if(strs[0][i] == strs[n][i]) ans.push_back(strs[0][i]);
             else break;
         }
         return ans;
