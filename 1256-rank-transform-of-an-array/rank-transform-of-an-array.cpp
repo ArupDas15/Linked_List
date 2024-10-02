5883fedbc389a515d4d8519618bade51f475d95f
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-02 19:30:33 +0530

    Time: 60 ms (81.51%) | Memory: 44.4 MB (32.37%) - LeetSync

--- 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
+++ 1256-rank-transform-of-an-array/rank-transform-of-an-array.cpp
@@ -5,14 +5,12 @@ public:
         std::unordered_map<int, int> ump;
         vector<int> ans;
 
-        if (arr.size() == 0) return ans;
         sort(copy_arr.begin(),copy_arr.end());
-        ump[copy_arr[0]] = 1;
-        for(int i = 1, rank = 2; i < copy_arr.size(); i++) {
-            if (copy_arr[i] > copy_arr[i - 1]) {
-                ump[copy_arr[i]] = rank;
+        for(int i = 0, rank = 1; i < copy_arr.size(); i++) {
+            if (i > 0 && (copy_arr[i] > copy_arr[i - 1])) {
                 rank++; 
             }
+            ump[copy_arr[i]] = rank;
         }
         for(int i = 0; i < arr.size(); i++) {
             ans.push_back(ump[arr[i]]);
