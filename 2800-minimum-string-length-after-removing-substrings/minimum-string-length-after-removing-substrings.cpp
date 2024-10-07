ebde2988b0bec222c8ffe02633282905114d80e9
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-07 08:27:37 +0530

    Time: 19 ms (5.57%) | Memory: 10.1 MB (91.13%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
     int minLength(string s) {
-        string final_string;
-        bool not_minimum = true;
-        do {
-            not_minimum = false;
-            final_string.clear();
-             for (int i = 0; i < s.length(); i++) {
-            if (i < s.length() - 1 && ((s[i] == 'A' && s[i + 1] == 'B') || (s[i] == 'C' && s[i + 1] == 'D'))) {
-                // Skip the next character as well
-                not_minimum = true;  // We made a removal
-                i++;  // Skip the current and the next character
-            } else {
-                final_string.push_back(s[i]);  // Keep the current character
+    while (s.find("AB") != -1 || s.find("CD") != -1) {
+            if (s.find("AB") != -1) {
+                // Remove all occurrences of "AB"
+                s.erase(s.find("AB"), 2);
+            } else if (s.find("CD") != -1) {
+                // Remove all occurrences of "CD"
+                s.erase(s.find("CD"), 2);
             }
         }
-        s = final_string;  // Update the string for the next iteration
-        } while (not_minimum);
         return s.length();
     }
 };
