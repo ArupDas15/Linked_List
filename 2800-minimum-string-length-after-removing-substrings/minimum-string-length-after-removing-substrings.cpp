5b39e0b3f7b002e806e30a962581422ef4f43350
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-07 08:20:15 +0530

    Time: 9 ms (24.64%) | Memory: 11.2 MB (77.73%) - LeetSync

---
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minLength(string s) {
+    string final_string;
+    bool not_minimum = true;
+
+    while (not_minimum) {
+        not_minimum = false;
+        final_string.clear();  // Clear the final string for new construction
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && ((s[i] == 'A' && s[i + 1] == 'B') || (s[i] == 'C' && s[i + 1] == 'D'))) {
+                // Skip the next character as well
+                not_minimum = true;  // We made a removal
+                i++;  // Skip the current and the next character
+            } else {
+                final_string.push_back(s[i]);  // Keep the current character
+            }
+        }
+        s = final_string;  // Update the string for the next iteration
+    }
+
+    return s.length();
+    }
+};
