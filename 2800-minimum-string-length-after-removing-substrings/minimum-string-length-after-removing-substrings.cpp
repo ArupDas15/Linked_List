11b0b07a1c3915cd5c0fd732e54a44b9670f9fa8
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-13 01:45:43 +0530

    Time: 19 ms (8.78%) | Memory: 10.1 MB (89.08%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    /* STEPS
+    1. Iterste until string.find("AB") or string.find("CD") is not -1.
+        1. Replace string.replace(string.find("AB"),"AB",1,"")
+        2. Replace string.replace(string.find("CD"),"CD",1,"")
+    2. Return string.length()
+    */
     int minLength(string s) {
-    while (s.find("AB") != -1 || s.find("CD") != -1) {
-            if (s.find("AB") != -1) {
-                // Remove all occurrences of "AB"
+        while(s.find("AB") != -1 or s.find("CD") != -1) {
+            if(s.find("AB") != -1) {
                 s.erase(s.find("AB"), 2);
-            } else if (s.find("CD") != -1) {
-                // Remove all occurrences of "CD"
+            }
+            if(s.find("CD") != -1) {
                 s.erase(s.find("CD"), 2);
             }
         }
