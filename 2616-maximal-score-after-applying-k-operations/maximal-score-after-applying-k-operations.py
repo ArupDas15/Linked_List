cb7754efcb3e00b4c3f94bd25a4861cefd226986
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-14 08:33:12 +0530

    Time: 778 ms (14.87%) | Memory: 31.3 MB (20.57%) - LeetSync

---
+++ 2616-maximal-score-after-applying-k-operations/maximal-score-after-applying-k-operations.py
@@ -0,0 +1,16 @@
+class Solution:
+    # NOTE: Python uses min heap by default 
+    # where as C++ uses max heap bby default.
+    # Time Complexity: O(klogn)
+    # Space Complexity: O(n)
+    def maxKelements(self, nums: List[int], k: int) -> int:
+        max_heap = []
+        sum = 0
+        for i in range(len(nums)):
+            heapq.heappush(max_heap, -nums[i])
+        while(k):
+            k -= 1
+            ele = -heapq.heappop(max_heap)
+            sum += ele
+            heapq.heappush(max_heap, -ceil(ele / 3))
+        return sum
