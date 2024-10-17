bf4bd51eb50436ccc8a8c59d5267777759d1d496
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-10-17 23:11:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (31.77%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    /* STEPS
+    1. Scan the integer from right to left
+       and store the index of the max digit from the current position 
+       which can be swapped in a vector named maxRightIdx.
+    2. Scan the integer from left to right
+       and if the digit at current position i is 
+       less than the digit at maxRightIdx[i] then perform 
+       the swap and output the ans.
+    */
+    int maximumSwap(int num) {
+        string numStr = to_string(num);
+        int n = numStr.length();
+        vector<int> maxRightIdx (n,-1);
+        int ans = num;
+
+        // First pass
+        maxRightIdx[n - 1] = n - 1;
+        for(int i = n - 2; i >= 0; i--) {
+            maxRightIdx[i] = 
+                (numStr[i] > numStr[maxRightIdx[i + 1]]) 
+                ? i : maxRightIdx[i + 1];
+        }
+
+        // Second pass
+        for(int i = 0; i < n; i++) {
+            if (numStr[i] < numStr[maxRightIdx[i]]) {
+                swap(numStr[i], numStr[maxRightIdx[i]]);
+                ans = stoi(numStr);
+                break;
+            }
+        }
+        return ans;
+    }
+};
