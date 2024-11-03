31d1a7c2d01eb9244b9ec6633215d0eaf1cd275b
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-03 20:01:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (55.81%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -3,6 +3,6 @@ public:
     bool rotateString(string s, string goal) {
         string newgoal = goal + goal;
         return ((s.length() == goal.length()) and 
-                (newgoal.find(s) != std::string::npos));
+                newgoal.contains(s));
     }
 };
