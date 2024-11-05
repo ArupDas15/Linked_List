3967e5231c822a0fe8ddb678d799ed2c4c7cd122
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-05 08:36:46 +0530

    Update minimum-number-of-changes-to-make-binary-string-beautiful.cpp

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -2,6 +2,15 @@ class Solution {
 public:
     // Time Complexity: O(n/2) ~ O(n)
     // Space Complexity: O(1)
+    /*  ---------------------------------------------------------------------------------
+        We can make the implementation much more concise by making a
+        key observation: any even-length sequence can be split into 
+        pairs of two characters. This is the smallest valid even sequence 
+        we can have. 
+        we can decompose the whole string into disjoint blocks of size 2 
+        and find the minimum number of changes required to make those blocks beautiful.
+        ---------------------------------------------------------------------------------
+    */
     int minChanges(string s) {
         int count = 0;
         for(int i = 0; i < s.length(); i+=2) {
