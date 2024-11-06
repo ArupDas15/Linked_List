d7e3fc6f5b217c3d16b1cc43ed27f17e889db224
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-06 08:45:58 +0530

    Time: 7 ms (24.46%) | Memory: 34.1 MB (11.97%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    // Brian Kernighan's Algorithm to 
+    // count the number of set bits (1s) in an integer's binary representation.
+    // Time Complexity: O(log n)
+    // Space Complexity: O(1)
+    int countSetBits(int n) {
+        int count = 0;
+        while (n != 0) {
+            n = n & (n - 1);
+            count++;
+        }
+        return count;
+    }
+
+    bool canSortArray(vector<int>& nums) {
+        vector<vector<int>>segments;
+        int cur_setbitcount = 0;
+        int prev_setbitcount = 0;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            cur_setbitcount = countSetBits(nums[i]);
+            if (prev_setbitcount == cur_setbitcount) {
+                segments.back().push_back(nums[i]);
+            } else {
+                vector<int> segment;
+                segment.push_back(nums[i]);
+                segments.push_back(segment);
+                prev_setbitcount = cur_setbitcount;   
+            }
+        }
+        for (int i = 0 ; i < segments.size() - 1; i++) {
+            if (*max_element(segments[i].begin(),segments[i].end())
+            > *min_element(segments[i+1].begin(),segments[i+1].end())) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
