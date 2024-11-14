dc4564727c96230361398d193727ed7fc041190d
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-14 17:37:38 +0530

    Time: 0 ms (100.00%) | Memory: 70.7 MB (22.78%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,32 +1,24 @@
 class Solution {
 public:
-    int maxCrossSubArraySum(vector<int>& nums, int l, int r, int mid) {
-        int temp_sum = 0;
-        int cross_sum = 0;
-        int max_left_sub_sum = INT_MIN;
-        int max_right_sub_sum = INT_MIN;
-
-        for(int i = mid; i >= l; i--) {
-            temp_sum += nums[i];
-            max_left_sub_sum = max(max_left_sub_sum, temp_sum);
-        }
-        temp_sum = 0;
-        for(int i = mid + 1; i <= r; i++) {
-            temp_sum += nums[i];
-            max_right_sub_sum = max(max_right_sub_sum, temp_sum);
-        }
-        cross_sum = max_left_sub_sum + max_right_sub_sum;
-        return cross_sum;
-    }
-    int maxSubArraySum(vector<int>& nums, int l, int r) {
-        if(l == r) return nums[l];
-        int mid = l + ((r - l) / 2);
-        int left_sum =  maxSubArraySum(nums, l, mid);
-        int right_sum = maxSubArraySum(nums, mid + 1, r);
-        int cross_sum = maxCrossSubArraySum(nums, l, r, mid);
-        return max({left_sum, right_sum, cross_sum});
-    }
+    /* ALGORITHM
+    1. Initialise a variable maxSum = INT_MIN
+    2. Initialise sum to 0.
+    3. Run a loop from 0 to nums.size() - 1.
+       3.1. sum = sum + nums[i] >= 0
+                maxSum = max(sum, maxSum);
+    4. Return maxSum.
+    Time Complexity: O(N)
+    Space Complexity: O(1)
+    */
     int maxSubArray(vector<int>& nums) {
-        return maxSubArraySum(nums, 0, nums.size() - 1);
+        int maxSum = INT_MIN, sum = 0;
+        for(int num : nums) {
+            sum = sum + num;
+            maxSum = max(maxSum, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxSum;
     }
 };
