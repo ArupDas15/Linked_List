40ef827a71a5e2ac327ba8b1dcccf0bb6c9b46a2
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-14 13:21:19 +0530

    Time: 1 ms (86.86%) | Memory: 14.4 MB (20.62%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    /* ALGORITHM:
-    1. create a dictionary umap of key-> nums[i], val -> index (i), initalise ans = []
-    2. iterate over nums:
-        2.1. if umap[target - nums[i]] and umap[target - nums[i]] != current num index exists.
-            2.1.1. store umap[nums[i]].val and umap[target - nums[i]].val in ans
-    3. return ans 
+    /* ALGORITHM
+    1. We create a hash table with key-> nums[i] and val-> index (i)
+    2. During every insert in the hash table we check if target-nums[i] already esits.
+        2.1.1 If it exists we return the pair, otherwise we insert nums[i].
     Time complexity: O(n)
     Space complexity: O(n)
-    */ 
+    */
     vector<int> twoSum(vector<int>& nums, int target) {
-        // Map: key-> nums[i], val -> index (i)
+        // key-> nums[i] and val-> index (i)
         unordered_map<int,int> umap;
-        vector<int> ans(2,0);
-        // Time compleity: O(n)
         for(int index = 0; index < nums.size(); index++) {
-            umap[nums[index]] = index;
-        }
-        for(int i = 0; i < nums.size(); i++) {
-            if ((umap.find(target - nums[i]) != umap.end()) && 
-                (umap[target - nums[i]] != i)) {
-                ans[0] = i;
-                ans[1] = umap[target - nums[i]];
-                break;
+            if (umap.find(target - nums[index]) != umap.end()) {
+                return vector<int> {
+                    index, umap[target - nums[index]]
+                    };
+            } else {
+                umap[nums[index]] = index;
             }
         }
-        return ans;
+        return vector<int> ();
+
+
     }
 };
