b57fad373574064ae566826526e797075d4175e7
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-14 15:40:43 +0530

    Time: 8 ms (5.65%) | Memory: 96.3 MB (6.34%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
+    /*ALGORITHM
+    1. create a veriable minbp for denoting minimum stock buying price
+    2. initialise minbp = prices[0], profit = 0
+    3. Run a  loop from 1 to prices.size()
+        3.1. cur_profit = prices[i] - minbp
+        3.2. profit = max(profit, cur_profit)
+        3.3. minbp = min(minbp, prices[i])
+    4. return profit
+    Time Complexity: O(n)
+    Space complexity: O(n)
+    */
     int maxProfit(vector<int>& prices) {
-        if(prices.size()==0){
-            return 0;
-        }
-        int mini = prices[0];
-        int maxProfit = 0;
-        int n = prices.size();
+        int profit = 0;
+        int minbp = prices[0];
 
-        for(int i = 1; i < n; i++) {
-            int cost = prices[i] - mini;
-            maxProfit = max(maxProfit, cost);
-            mini = min(mini, prices[i]);
+        for(int i = 1; i < prices.size(); i++) {
+            int cur_profit = prices[i] - minbp;
+            profit = max(profit, cur_profit);
+            minbp = min(minbp, prices[i]);
         }
-        return maxProfit;
+        return profit;
     }
 };
