6f6c996c3d02d7a7b1f48310d7e30563adace84e
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-14 17:04:14 +0530

    Time: 0 ms (100.00%) | Memory: 38.4 MB (55.64%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
+    /* ALGORITHM
+       1. Initialise prefixProduct = suffixProduct = 1
+       2. Initialise a vector prefixSuffixProduct to store the product.
+       --- PREFIX PRODUCT CALCULATION
+       3. Iterate over all elements of nums from 0 to nums.size()-1
+          3.1. The prefix of an element is the product until the previous element.
+               The prefix product of element at index 0 is 1.
+       --- SUFFIX PRODUCT CALCULATION
+       4. Iterate over all elements of nums from nums.size() - 1 to 0.
+          4.1. The suffix of an element is the product after the current element.
+               The suffix product of an element at index nums.size() - 1 is 1.
+       5. Multiply the vectors prefix and suffix to find the prefixSuffixProduct vector.
+    */
+    // Time complexity: O(n)
+    // Space complexity: O(1)
     vector<int> productExceptSelf(vector<int>& nums) {
-        int length = nums.size();
-        vector<int> products(length);
-
+        vector<int> prefixSuffixProduct(nums.size(), 1);
         int prefixProduct = 1;
-        for (int i = 0; i < length; i++) {
-            products[i] = prefixProduct;
+        int suffixProduct = 1;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            prefixSuffixProduct[i] *= prefixProduct;
             prefixProduct *= nums[i];
         }
-
-        int suffixProduct = 1;
-        for (int i = length - 1; i >= 0; i--) {
-            products[i] *= suffixProduct;
+        cout<<endl;
+        for(int i = nums.size() - 1; i >= 0; i--) {
+            prefixSuffixProduct[i] *= suffixProduct; 
             suffixProduct *= nums[i];
         }
-
-        return products;
+        return prefixSuffixProduct;
     }
 };
