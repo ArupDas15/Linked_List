ecfdeb625140d578f0e91a8aa6da0fd285be865f
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-16 20:38:16 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (5.40%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -0,0 +1,24 @@
+class Solution:
+    """ There is a point in the array called the inflection point at which:
+            1. All the elements to the left of the inflection point are sorted.
+            2. All the elements to the right of the inflection point including 
+               the inflection point are sorted.
+        if nums[mid] < nums[right] then nums[mid ... right] is sorted. In this case,
+        we shift right to mid because we need to find the inflection point including nums[mid].
+        otherwise, left = mid + 1 because we know that nums[mid] is not the inflection point for sure.
+        when left == right we stop.
+    """
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+        mid = (left + right) // 2
+
+        while left != right:
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid + 1
+            mid = (left + right) // 2
+        return nums[mid]
