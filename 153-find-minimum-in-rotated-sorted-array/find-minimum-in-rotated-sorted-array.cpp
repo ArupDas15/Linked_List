bdff4d5ac2e2ac6c55e111cf1d077089abc06895
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-16 20:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (6.36%) - LeetSync

---
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    /*
+    There is a point in the array called the inflection point at which:
+            1. All the elements to the left of the inflection point are sorted.
+            2. All the elements to the right of the inflection point including 
+               the inflection point are sorted.
+        if nums[mid] < nums[right] then nums[mid ... right] is sorted. In this case,
+        we shift right to mid because we need to find the inflection point including nums[mid].
+        otherwise, left = mid + 1 because we know that nums[mid] is not the inflection point for sure.
+        when left == right we stop.
+    Time Complexity: O(log n) since we are using Binary Search.
+    Space Complexity: O(n)
+    */
+    int findMin(vector<int>& nums) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid = (left + right) / 2;
+
+        while(left != right) {
+            // check if nums[mid ... right] is sorted.
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+            mid = (left + right) / 2;
+        }
+        return nums[mid];
+    }
+};
