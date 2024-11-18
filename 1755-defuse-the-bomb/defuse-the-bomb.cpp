c03f78ef76a29409ee589d53704b1db1de50daf5
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-18 21:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (84.00%) - LeetSync

--- 1755-defuse-the-bomb/defuse-the-bomb.cpp
+++ 1755-defuse-the-bomb/defuse-the-bomb.cpp
@@ -1,5 +1,13 @@
 class Solution {
 public:
+    // Time complexity: O(n^2)
+    // Space complexity: O(n)
+    // Observe in this question the contraints are below 500 that is they are really very small.
+    // so that gives an indication that the Brute-Force solution should work on this problem.
+    // The only tricky part here is when k < 0.
+    // (i - cnt + n) % n where cnt is say absolute value of k (when k < 0). thus i -cnt is negative
+    // (i.e. you are moving pasth the begining of the array), adding n ensure a non-negative index 
+    // and the module operation wraps the index correctly for the circular array.
     vector<int> decrypt(vector<int>& code, int k) {
         vector<int> ans;
         int n  = code.size();
