ea3ae64343af1c6df1145bda782ae254006d29e6
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-19 00:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (33.99%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     // Time Complexity: O(log n)
     // Space Complexity: O(1)
+    // Approach: To find the hamming weight we keep storing the 
+    // least significant set bit until the number n reduces to 0.
+    // Use Brian-Kerningham algorithm
     int hammingWeight(uint32_t n) {
         int hamming_wt = 0;
+
         while(n > 0) {
             hamming_wt++;
             n &= (n - 1);
