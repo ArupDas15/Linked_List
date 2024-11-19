b81331c5aca74f7ba7a4cb77dd114ce55ec51031
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-11-19 09:29:59 +0530

    Time: 138 ms (36.67%) | Memory: 36.8 MB (6.50%) - LeetSync

---
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -0,0 +1,35 @@
+class Solution:
+    """
+    ALGORITHM
+    1. create a hashmap nums_to_idx = {}
+    2. initialise curSum to 0, ans = float('-inf')
+    3. initialise begin = end = 0
+    4. while end < len(nums):
+        4.2. curSum = curSum + nums[end]
+        4.2. while begin < nums_to_idx.get(nums[end], -1) or end-begin+1 > k:
+            4.2.1. curSum -= nums[begin]
+            4.2.2. increment begin
+        4.3. nums_to_idx[nums[end]] = end
+        4.4. if end-begin+1==k: ans = max(ans,curSum)
+        4.5. increment end
+    """
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        nums_to_idx = dict()
+        curSum = 0
+        begin = end = 0
+        ans = 0
+
+        while end < len(nums):
+            curSum = curSum + nums[end]
+            while begin <= nums_to_idx.get(nums[end], -1) or end-begin+1 > k:
+                curSum -= nums[begin]
+                begin += 1
+            # nums[end] is unique so insert it into the hashmap
+            nums_to_idx[nums[end]] = end
+            # check if window size is k and update ans
+            if end - begin + 1 == k:
+                ans = max(ans, curSum)
+            end += 1
+        return ans
