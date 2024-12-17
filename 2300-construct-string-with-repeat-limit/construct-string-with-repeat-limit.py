f668541b7e4b40c4be2d8b2e197f9842de2ed383
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-12-17 17:59:51 +0530

    Time: 197 ms (76.45%) | Memory: 19.7 MB (9.44%) - LeetSync

--- 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
+++ 2300-construct-string-with-repeat-limit/construct-string-with-repeat-limit.py
@@ -1,36 +1,29 @@
 """
-Time complexity: O(n) for counting frequency
-O(n*25) where n is the no. of characters in s.
-Because the outer while loop will run atmost 26 times. THe inner loop which finds the next large character will
-run 25 times in worst case assuming the next big character in s is 'a'. If the repeat limit is 1, then after inserting
-a single character in result we have to use the inner loop.
+Time complexity: To determine the frequency is O(n) where n is length of string s
+heapify operation will take O(n) time.
+The max heap will contain k nodes where k is unique characters in s
+If we perform a heap pop and heap push for every character in the string in worst case assuming the repeatLimit is 1
+then we perform O(n) heap operations with every heap operation taking O(log k) time.
+Thus, the time complexity is: O(nlogk)
+
+Space complexity: O(log k) because there are only k unique characters so there will be only k nodes.
 """
 class Solution:
     def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
-        freq = [0] * 26
+        max_heap = [(-ord(c), cnt) for c, cnt in Counter(s).items()]
+        heapq.heapify(max_heap)
         result = []
-        current_char_idx = 25 # initially ponts to 'z'
 
-        for char in s:
-            freq[ord(char) - ord('a')] += 1
-
-        while current_char_idx >= 0:
-            if freq[current_char_idx] == 0:
-                current_char_idx -= 1
-                continue
-            else:
-                use = min(freq[current_char_idx], repeatLimit)
-                result.append(chr(current_char_idx + ord('a')) * use)
-                freq[current_char_idx] -= use
-
-                if freq[current_char_idx] > 0:
-                    next_large_char_idx = current_char_idx - 1
-                    while next_large_char_idx >= 0 and freq[next_large_char_idx] == 0:
-                        # Finds the next large character with non zero frequency
-                        next_large_char_idx -= 1
-                    if next_large_char_idx < 0:
-                        break # construction of result further is not possible
-                    result.append(chr(next_large_char_idx + ord('a')))
-                    freq[next_large_char_idx] -= 1
-        
+        while max_heap:
+            large_char, cnt = heapq.heappop(max_heap)
+            char = chr(-large_char)
+            use = min(cnt, repeatLimit)
+            result.append(char * use)
+            if cnt > use and max_heap:
+                next_large_char, next_cnt = heapq.heappop(max_heap)
+                result.append(chr(-next_large_char))
+                if next_cnt > 1:
+                    heapq.heappush(max_heap,(next_large_char, next_cnt-1))
+                heapq.heappush(max_heap, (large_char, cnt-use))
+         
         return "".join(result)
