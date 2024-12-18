90b0355ab1a9c4d39905682792ad0f0bb0b700ed
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-12-19 01:24:14 +0530

    Time: 8 ms (68.17%) | Memory: 18.4 MB (9.18%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,17 +1,23 @@
 class Solution:
-    # Time complexity: O(n)
-    # Space complexity: O(1)
+    """
+    Since we have to find the largest subarray product 
+    (keep in mind **continuous** array and **largest** product),
+    we will find the prefix and suffix product and output the maximum out of the two
+    while iterating through nums. 
+    Time complexity : O(n)
+    Space Complexity: O(1)
+    """
     def maxProduct(self, nums: List[int]) -> int:
-        ans = float('-inf')
-        prefix = 1
-        suffix = 1
+        prefix = suffix = 1
+        ans = -float('inf')
 
         for i in range(len(nums)):
             prefix *= nums[i]
-            suffix = suffix * nums[len(nums) - i - 1]
-            ans = max(ans, max(prefix, suffix))
+            suffix *= nums[len(nums) - i - 1]
+            ans = max(ans, prefix, suffix)
             if prefix == 0:
                 prefix = 1
             if suffix == 0:
                 suffix = 1
         return ans
+        
