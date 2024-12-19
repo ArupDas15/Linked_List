47f1e0b0fe545178fa1522243b7f56c5b2a6a47c
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2024-12-19 16:46:42 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (10.70%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.py
@@ -4,7 +4,7 @@ class Solution:
     def findMin(self, nums: List[int]) -> int:
         left = 0
         right  = len(nums)-1
-        
+        mid = (left + right) // 2
         while left <= right:
             mid = (left + right) // 2
             if (nums[left] < nums[mid]) and (nums[mid] < nums[right]):
@@ -12,7 +12,7 @@ class Solution:
                 return nums[left]
             elif (nums[mid] < nums[right]):
                 """"nums[mid ... right] is sorted so we move right to mid, 
-                    when nums[mid] = mus[right] we have found the inflection point.
+                    when nums[mid] = nums[right] we have found the inflection point.
                     In the next iteration left = mid + 1 i.e. right + 1 and it will 
                     exit the while loop and return nums[mid]
                 """
