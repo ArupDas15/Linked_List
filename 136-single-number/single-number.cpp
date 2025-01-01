9677ecc649638f087fb8c2e0bf72286948805a37
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-01-01 19:14:19 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (44.50%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+    // time complexity: O(n)
+    // space complexity: O(n)
     int singleNumber(vector<int>& nums) {
         int ans = 0;
 
-        for(int x : nums) {
-            ans = ans ^ x;
+        for(int num : nums) {
+            ans = ans ^ num;
         }
         return ans;
     }
