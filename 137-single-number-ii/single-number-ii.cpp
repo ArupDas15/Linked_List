bdef5e17f54d65969dc8bb770258472dabeb9319
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-01-04 06:46:31 +0530

    Time: 3 ms (34.21%) | Memory: 14 MB (7.44%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        unordered_set<int> unique_nums_set(nums.begin(), nums.end());
+        vector<long long int>unique_nums_vec;
+        for (int num : unique_nums_set) {
+            unique_nums_vec.push_back(num);
+        }
+        long long int thrice_sum = 3*accumulate(unique_nums_vec.begin(), unique_nums_vec.end(), 0LL);
+        long long int arr_sum = accumulate(nums.begin(), nums.end(), 0LL);
+        return (thrice_sum - arr_sum)/2;
+    }
+};
