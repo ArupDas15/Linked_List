e6cca71f856306c7daac498b5651fe568b238a3d
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-01-26 19:04:04 +0530

    Time: 1236 ms (50.09%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -3,21 +3,13 @@ SELECT
     p.product_id, 
     ROUND(
         COALESCE(
-            SUM(
-                CASE
-                    WHEN u.purchase_date between p.start_date and p.end_date 
-                    THEN p.price * u.units
-                END
-                )/SUM(CASE
-                    WHEN u.purchase_date between p.start_date and p.end_date 
-                    THEN u.units
-                END), 
+            SUM(p.price * u.units)/SUM(u.units), 
             0), 
         2) as average_price
 FROM 
     Prices as p 
     LEFT JOIN UnitsSold as u
-    ON p.product_id = u.product_id
+    ON p.product_id = u.product_id AND u.purchase_date between p.start_date and p.end_date
 group by 
     p.product_id
 order by 
