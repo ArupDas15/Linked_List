430a5538bdd4b369c13af866031a914385589608
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-01-27 11:06:46 +0530

    Time: 693 ms (32.58%) | Memory: 25.8 MB (85.71%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def lcs(self, ind1: int, ind2: int, s: str, t: str, dp: list[list[int]]) -> int:
+        if ind1 < 0 or ind2 < 0: return 0
+        if dp[ind1][ind2] != -1: return dp[ind1][ind2]
+        if s[ind1] == t[ind2]: dp[ind1][ind2] = 1 + self.lcs(ind1-1,ind2-1, s, t, dp)
+        else: dp[ind1][ind2] = max(self.lcs(ind1-1,ind2, s, t, dp), self.lcs(ind1,ind2-1, s, t, dp))
+        return dp[ind1][ind2]
+    
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[-1] * len(text2) for _ in range(len(text1))]
+        return self.lcs(len(text1) - 1, len(text2) - 1, text1, text2, dp)
