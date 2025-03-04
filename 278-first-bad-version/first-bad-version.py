34d4dd1c6fd7e22f734af6bb71dc07699c58aa7f
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-03-04 20:12:28 +0530

    Time: 32 ms (87.52%) | Memory: 17.6 MB (92.17%) - LeetSync

--- 278-first-bad-version/first-bad-version.py
+++ 278-first-bad-version/first-bad-version.py
@@ -12,7 +12,7 @@ class Solution:
             mid = l + (h - l) // 2
             if isBadVersion(mid):
                 h = mid - 1
-                ans = min(ans, mid)
+                ans = mid
             else: l = mid + 1
         return ans
         
