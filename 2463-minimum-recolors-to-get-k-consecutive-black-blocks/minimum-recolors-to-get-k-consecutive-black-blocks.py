2b6f9b5135669c20fd7be7a089b2857febf56a5d
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-03-09 13:22:40 +0530

    Time: 3 ms (30.79%) | Memory: 17.8 MB (38.43%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,20 +1,18 @@
 class Solution:
+    def recursiveSoln(self, l, r, blocks, count, ans, k):
+        # Base Case
+        if r == len(blocks): return ans
+        if blocks[r] == 'W':
+            count +=1
+        if r - l +  1 == k:
+            ans = min(ans, count)
+            if blocks[l] == 'W': count -= 1
+            return self.recursiveSoln(l+1, r+1, blocks, count, ans, k)
+        return self.recursiveSoln(l, r+1, blocks, count, ans, k)
+    
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        l = 0
-        r = 0
-        count = 0
-        ans = len(blocks)
+        return self.recursiveSoln(0,0, blocks, 0, k, k)
 
-        def recursiveSoln(l, r, blocks, count, ans):
-            # Base Case
-            if r == len(blocks): return ans
-            if blocks[r] == 'W':
-                count +=1
-            if r - l +  1 == k:
-                ans = min(ans, count)
-                if blocks[l] == 'W': count -= 1
-                return recursiveSoln(l+1, r+1, blocks, count, ans)
-            return recursiveSoln(l, r+1, blocks, count, ans)
-        return recursiveSoln(0,0, blocks, 0, k)    
+    
 
         
