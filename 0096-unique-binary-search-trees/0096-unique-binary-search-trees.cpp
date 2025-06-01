681305a4a33945f6e7f2a9665c5212b85275e6cc
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2025-06-01 12:42:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (90.31%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
-    vector<int> cat;
-    int numTrees(int n) {
-        cat.push_back(1);
-        cat.push_back(1);
-        
-        for(int i = 2; i <= n; i++) {
-            cat.push_back(0);
-            for(int j = 0; j < i; j++)
-            cat[i] += (cat[j]*cat[i-j-1]);
+    // vector<int> cat;
+    unsigned long int binCoeff(int n, int r) {
+        if (r > n - r) {
+            r = n - r;
         }
-        return cat[n];
+        
+        unsigned long int res = 1;
+
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i+1);
+        }
+
+        return res;
+    }
+
+    int numTrees(int n) {
+        // cat.push_back(1);
+        // cat.push_back(1);
+        
+        // for(int i = 2; i <= n; i++) {
+        //     cat.push_back(0);
+        //     for(int j = 0; j < i; j++)
+        //     cat[i] += (cat[j]*cat[i-j-1]);
+        // }
+        // return cat[n];
+        return binCoeff(2*n,n)/(n+1);
     }
 };
