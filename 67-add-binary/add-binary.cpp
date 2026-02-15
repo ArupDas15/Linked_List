fd0c07f7a813bf917462fef815737a5fcf61d8ba
Arup Das <37553488+ArupDas15@users.noreply.github.com>
2026-02-15 11:47:22 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (36.83%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,29 +1,19 @@
 #define CHAR_TO_INT(c) ((c)-'0')
 class Solution {
 public:
-    string add_zeros(string b, int zero_pad_len) {
-        string temp;
-        for(int i = 0; i < zero_pad_len; i++) {
-            temp.push_back('0');
-        }
-        temp = temp+b;
-        return temp;
-    }
     string addBinary(string a, string b) {
-       int sum = 0, carry = 0;
-       string ans;
-
-       if(a.length() != b.length()) {
-            if(a.length()> b.length()) {
-                b = add_zeros(b, a.length() - b.length());
-            } else {
-                a = add_zeros(a, b.length() - a.length());
-            }
-        }
-        for(int i = a.length()-1; i >= 0; i--) {
-            sum = CHAR_TO_INT(a[i]) + CHAR_TO_INT(b[i]) + carry;
+        int sum = 0, carry = 0;
+        string ans;
+        int i = a.length()-1;
+        int j = b.length()-1;
+        
+        while(i>=0||j>=0) {
+            if(i>=0) sum += CHAR_TO_INT(a[i--]);
+            if(j>=0) sum += CHAR_TO_INT(b[j--]);
+            sum += carry;
             ans.push_back((sum % 2) + '0');
             carry = sum/2;
+            sum = 0;
         }
         if (carry == 1) ans.push_back('1');
         reverse(ans.begin(), ans.end());
